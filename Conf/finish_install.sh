89f9f3d4d765055412d90d8c9aca9ae978e6557e
Tom Burgin <tburgin@users.noreply.github.com>
2024-10-10 10:50:56 -0400

    add pre and post install bits to finish_install.sh

--- Conf/finish_install.sh
+++ Conf/finish_install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Move the .app into place, install and load supporting services.
-# If a user is logged in, also load the GUI agent.
+# This script is run by the NPS Santa sysx (com.northpolesec.santa.daemon) on
+# sysx startup, if the Google Santa sysx (com.google.santa.daemon) has been
+# removed.
+# The script removes the rest of Google Santa's services and on-disk
+# artifacts, then finishes installing NPS Santa.
 
 function die {
   echo "${@}"
@@ -9,13 +12,40 @@ function die {
 }
 
 # CONF_DIR is a required environment variable that points the directory that
-# launch job configurations. In the intended use case, CONF_DIR points to the
-# running system extension's Resource directory.
+# contains NPS Santa launch job configurations. In the intended use case,
+# CONF_DIR points to the com.northpolesec.santa.daemon Resource directory.
 [[ -n "${CONF_DIR}" ]] || die "CONF_DIR unset"
 
-# Finish installing the .app.
+################################################################################
+
+# Remove Google Santa's services and on-disk artifacts.
+/bin/launchctl remove com.google.santa.bundleservice || true
+/bin/launchctl remove com.google.santa.metricservice || true
+/bin/launchctl remove com.google.santa.syncservice || true
+GUI_USER=$(/usr/bin/stat -f '%u' /dev/console)
+[[ -n "${GUI_USER}" ]] && /bin/launchctl asuser "${GUI_USER}" /bin/launchctl remove com.google.santa || true
 /bin/rm -rf /Applications/Santa.app
+/bin/rm -f /Library/LaunchAgents/com.google.santa.plist
+/bin/rm -f /Library/LaunchDaemons/com.google.santa.bundleservice.plist
+/bin/rm -f /Library/LaunchDaemons/com.google.santa.metricservice.plist
+/bin/rm -f /Library/LaunchDaemons/com.google.santa.syncservice.plist
+/bin/rm -f /private/etc/asl/com.google.santa.asl.conf
+/bin/rm -f /private/etc/newsyslog.d/com.google.santa.newsyslog.conf
+
+################################################################################
+
+# Unload NPS Santa services in preparation for the update.
+/bin/launchctl remove com.northpolesec.santa.bundleservice || true
+/bin/launchctl remove com.northpolesec.santa.metricservice || true
+/bin/launchctl remove com.northpolesec.santa.syncservice || true
+[[ -n "${GUI_USER}" ]] && /bin/launchctl asuser "${GUI_USER}" /bin/launchctl remove com.northpolesec.santa || true
+
+# Finish installing NPS Santa.
 /bin/mv /Applications/Santa_NPS.app /Applications/Santa.app
+
+# Create a symlink for santactl.
+mkdir -p /usr/local/bin
+/bin/ln -sf /Applications/Santa.app/Contents/MacOS/santactl /usr/local/bin/santactl
 
 # Install the launch jobs.
 /bin/cp -vX "${CONF_DIR}/com.northpolesec.santa.plist" "/Library/LaunchAgents/"
@@ -23,17 +53,10 @@ function die {
 /bin/cp -vX "${CONF_DIR}/com.northpolesec.santa.metricservice.plist" "/Library/LaunchDaemons/"
 /bin/cp -vX "${CONF_DIR}/com.northpolesec.santa.syncservice.plist" "/Library/LaunchDaemons/"
 
-# Load com.northpolesec.santa.bundleservice
+# Load the services.
 /bin/launchctl load -w /Library/LaunchDaemons/com.northpolesec.santa.bundleservice.plist
-
-# Load com.northpolesec.santa.metricservice
 /bin/launchctl load -w /Library/LaunchDaemons/com.northpolesec.santa.metricservice.plist
-
-# Load com.northpolesec.santa.syncservice
 /bin/launchctl load -w /Library/LaunchDaemons/com.northpolesec.santa.syncservice.plist
+[[ -n "${GUI_USER}" ]] && /bin/launchctl asuser "${GUI_USER}" /bin/launchctl load /Library/LaunchAgents/com.northpolesec.santa.plist
 
-GUI_USER=$(/usr/bin/stat -f '%u' /dev/console)
-[[ -z "${GUI_USER}" ]] && exit 0
-
-/bin/launchctl asuser "${GUI_USER}" /bin/launchctl load /Library/LaunchAgents/com.northpolesec.santa.plist
 exit 0
