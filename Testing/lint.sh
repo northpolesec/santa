cf7c32706a0ea92af9c2551f2ccb7ae055d5dc12
Pete Markowsky <peterm@vodun.org>
2024-10-11 20:25:10 -0400

    Conditionally check for virtualenv and install it if it's missing

--- Testing/lint.sh
+++ Testing/lint.sh
@@ -10,6 +10,20 @@ find ${GIT_ROOT} \( -name "*.m" -o -name "*.h" -o -name "*.mm" -o -name "*.cc" \
 go install github.com/bazelbuild/buildtools/buildifier@latest
 ~/go/bin/buildifier --lint=warn -r ${GIT_ROOT}
 
+if command -v virtualenv &> /dev/null
+then
+    echo ""
+else
+    echo "virtualenv not found. Installing..."
+    sudo pip3 install virtualenv
+    if [ $? -eq 0 ]; then
+        echo "virtualenv has been successfully installed"
+    else
+        echo "Failed to install virtualenv"
+        exit 1
+    fi
+fi
+
 virtualenv ./venv
 source ./venv/bin/activate
 python3 -m pip install -q pyink
