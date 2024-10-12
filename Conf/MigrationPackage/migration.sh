53bf9c8df48e3c8b7d78bfe53f3f3505764ac8fd
Tom Burgin <tburgin@users.noreply.github.com>
2024-10-12 19:31:14 -0400

    installer: generate migration pkg [2/2] (#30)

--- Conf/MigrationPackage/migration.sh
+++ Conf/MigrationPackage/migration.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
 
-# TODO: Add migration logic
-while true; do logger "migration"; sleep 1; done;
+while true;
+do
+  GOOGLE_SANTA_ACTIVATED="$(/usr/bin/systemextensionsctl list com.apple.system_extension.endpoint_security |
+                            /usr/bin/grep -E '^.+EQHXZ8M8AV.+com\.google\.santa\.daemon.+activated.+$')"
+  if [ -z "${GOOGLE_SANTA_ACTIVATED}" ]; then
+    break;
+  else
+    sleep 10
+  fi
+done
+
+# Remove Google Santa's services and on-disk artifacts.
+/bin/launchctl remove com.google.santa.bundleservice || true
+/bin/launchctl remove com.google.santa.metricservice || true
+/bin/launchctl remove com.google.santa.syncservice || true
+GUI_USER=$(/usr/bin/stat -f '%u' /dev/console)
+[[ -n "${GUI_USER}" ]] && /bin/launchctl asuser "${GUI_USER}" /bin/launchctl remove com.google.santa || true
+/bin/rm -rf /Applications/Santa.app
+/bin/rm -f /Library/LaunchAgents/com.google.santa.plist
+/bin/rm -f /Library/LaunchDaemons/com.google.santa.bundleservice.plist
+/bin/rm -f /Library/LaunchDaemons/com.google.santa.metricservice.plist
+/bin/rm -f /Library/LaunchDaemons/com.google.santa.syncservice.plist
+/bin/rm -f /private/etc/asl/com.google.santa.asl.conf
+/bin/rm -f /private/etc/newsyslog.d/com.google.santa.newsyslog.conf
+
+# Install NPS Santa.
+/usr/sbin/installer -pkg /Library/Caches/com.northpolesec.santa/santa.pkg -target /
+
+# Cleanup migration service and on-disk artifacts.
+/bin/rm /Library/Caches/com.northpolesec.santa/santa.pkg
+/bin/rm /Library/Caches/com.northpolesec.santa/migration.sh
+/bin/rm /Library/LaunchDaemons/com.northpolesec.santa.migration.plist
+/bin/launchctl remove com.northpolesec.santa.migration
+
+exit 0
