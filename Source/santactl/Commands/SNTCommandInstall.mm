11dc1a877d40c08a86b556acf898dffb66ef2014
Matt W <436037+mlw@users.noreply.github.com>
2024-10-16 11:09:14 -0400

    PR feedback

--- Source/santactl/Commands/SNTCommandInstall.mm
+++ Source/santactl/Commands/SNTCommandInstall.mm
@@ -36,7 +36,7 @@ REGISTER_COMMAND_NAME(@"install")
 }
 
 + (NSString *)shortHelpText {
-  return @"Install a given package";
+  return @"Instruct the daemon to install Santa.app."
 }
 
 + (NSString *)longHelpText {
