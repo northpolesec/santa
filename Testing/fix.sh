28db503f5f7d2eb108216016d117af7a3c8f318f
Russell Hancox <russellhancox@users.noreply.github.com>
2024-10-25 13:10:49 -0400

    Project: Disable pyink checks (#77)
    pyink isn't installed by default and isn't available in homebrew, making
    using it a bit annoying.
    
    I've checked in a .pylintrc so we can manually lint if we want to
    (`pylint .` is enough), given that pylint seems to be installed by
    default when Homebrew installs python3. I didn't add this to the
    fix.sh/lint.sh scripts because pylint won't auto-fix problems and 2 of
    the scripts checked in do not validate.

--- Testing/fix.sh
+++ Testing/fix.sh
@@ -3,4 +3,3 @@ GIT_ROOT=$(git rev-parse --show-toplevel)
 
 find ${GIT_ROOT} \( -name "*.m" -o -name "*.h" -o -name "*.mm" -o -name "*.cc" \) -exec xcrun clang-format -i {} \+
 buildifier --lint=fix -r ${GIT_ROOT}
-python3 -m pyink --config ${GIT_ROOT}/.pyink-config ${GIT_ROOT}
