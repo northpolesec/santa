5b44565c9ed8dcea8d3e7a71ad549f412a04b8f2
Russell Hancox <russell@hancox.us>
2024-10-27 23:37:51 -0400

    swift format has bugs on linux

--- Testing/lint.sh
+++ Testing/lint.sh
@@ -5,7 +5,11 @@ GIT_ROOT=$(git rev-parse --show-toplevel)
 
 find ${GIT_ROOT} \( -name "*.m" -o -name "*.h" -o -name "*.mm" -o -name "*.cc" \) -exec clang-format --Werror --dry-run {} \+
 
-swift format -i -r ${GIT_ROOT}
+# Swift 6.0.1 included on the current Ubuntu 22.04 runner image has a bug that
+# causes an infinite loop when calling `swift format`. Until the image is updated
+# with a version of Swift without this problem, limit swift formatting checks to
+# macOS hosts.
+[[ "$(uname)" -eq "Darwin" ]] && swift format lint -r ${GIT_ROOT}
 
 ! git grep -EIn $'[ \t]+$' -- ':(exclude)*.patch'
 
