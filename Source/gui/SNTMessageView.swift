4513e0e3bee857bee71d65e39a5a139cc0d695f7
Russell Hancox <russell@hancox.us>
2024-10-31 16:34:39 -0400

    Restore saturation to 90%

--- Source/gui/SNTMessageView.swift
+++ Source/gui/SNTMessageView.swift
@@ -20,6 +20,7 @@ public struct SNTMessageView<Content: View>: View {
             .resizable()
             .frame(maxWidth: 32, maxHeight: 32)
             .offset(x: -75)
+            .saturation(0.9)
           Text(verbatim: "Santa").font(Font.custom("HelveticaNeue-UltraLight", size: 34.0))
         }
       }
