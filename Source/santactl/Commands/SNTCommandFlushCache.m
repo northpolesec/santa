4a4cae15b4285faa8b9565100b7c4223b09a5c81
Pete Markowsky <peterm@vodun.org>
2024-10-31 15:59:01 -0400

    Expose flushcache command

--- Source/santactl/Commands/SNTCommandFlushCache.m
+++ Source/santactl/Commands/SNTCommandFlushCache.m
@@ -12,8 +12,6 @@
 ///    See the License for the specific language governing permissions and
 ///    limitations under the License.
 
-#ifdef DEBUG
-
 #import <Foundation/Foundation.h>
 #import <MOLXPCConnection/MOLXPCConnection.h>
 
@@ -59,5 +57,3 @@ REGISTER_COMMAND_NAME(@"flushcache")
 }
 
 @end
-
-#endif
