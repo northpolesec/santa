northpolesec/santa

commit 089761c1d1ac88d58fb76d3d135566d0459251ac

santad/santactl: Add comment field for rules, populate for manual rules (#123)
Fixes #112

Source/santad/DataLayer/SNTRuleTable.m
@@ -242,6 +242,11 @@ static void addPathsFromDefaultMuteSet(NSMutableSet *criticalPaths) {
     newVersion = 7;
   }
 
+  if (version < 8) {
+    [db executeUpdate:@"ALTER TABLE 'rules' ADD 'comment' TEXT"];
+    newVersion = 8;
+  }
+
   // Save signing info for launchd and santad. Used to ensure they are always allowed.
   self.santadCSInfo = [[MOLCodesignChecker alloc] initWithSelf];
   self.launchdCSInfo = [[MOLCodesignChecker alloc] initWithPID:1];
@@ -313,7 +318,8 @@ static void addPathsFromDefaultMuteSet(NSMutableSet *criticalPaths) {
                                              state:[rs intForColumn:@"state"]
                                               type:[rs intForColumn:@"type"]
                                          customMsg:[rs stringForColumn:@"custommsg"]
-                                         timestamp:[rs intForColumn:@"timestamp"]];
+                                         timestamp:[rs intForColumn:@"timestamp"]
+                                           comment:[rs stringForColumn:@"comment"]];
   r.customURL = [rs stringForColumn:@"customurl"];
   return r;
 }
@@ -394,7 +400,8 @@ static void addPathsFromDefaultMuteSet(NSMutableSet *criticalPaths) {
                                          state:SNTRuleStateAllow
                                           type:SNTRuleTypeCertificate
                                      customMsg:nil
-                                     timestamp:0];
+                                     timestamp:0
+                                       comment:nil];
   }
 
   return rule;
@@ -435,11 +442,12 @@ static void addPathsFromDefaultMuteSet(NSMutableSet *criticalPaths) {
           return;
         }
       } else {
-        if (![db executeUpdate:@"INSERT OR REPLACE INTO rules "
-                               @"(identifier, state, type, custommsg, customurl, timestamp) "
-                               @"VALUES (?, ?, ?, ?, ?, ?);",
-                               rule.identifier, @(rule.state), @(rule.type), rule.customMsg,
-                               rule.customURL, @(rule.timestamp)]) {
+        if (![db
+              executeUpdate:@"INSERT OR REPLACE INTO rules "
+                            @"(identifier, state, type, custommsg, customurl, timestamp, comment) "
+                            @"VALUES (?, ?, ?, ?, ?, ?, ?);",
+                            rule.identifier, @(rule.state), @(rule.type), rule.customMsg,
+                            rule.customURL, @(rule.timestamp), rule.comment]) {
           [self fillError:error
                      code:SNTRuleTableErrorInsertOrReplaceFailed
                   message:[db lastErrorMessage]];

Source/common/SNTRule.h
@@ -53,13 +53,19 @@
 @property(readonly) NSUInteger timestamp;
 
 ///
+///  A comment attached to this rule. This is intended only for local rules.
+///
+@property(copy) NSString *comment;
+
+///
 ///  Designated initializer.
 ///
 - (instancetype)initWithIdentifier:(NSString *)identifier
                              state:(SNTRuleState)state
                               type:(SNTRuleType)type
                          customMsg:(NSString *)customMsg
-                         timestamp:(NSUInteger)timestamp;
+                         timestamp:(NSUInteger)timestamp
+                           comment:(NSString *)comment;
 
 ///
 ///  Initialize with a default timestamp: current time if rule state is transitive, 0 otherwise.

Source/common/SNTRule.m
@@ -33,7 +33,8 @@ static const NSUInteger kExpectedTeamIDLength = 10;
                              state:(SNTRuleState)state
                               type:(SNTRuleType)type
                          customMsg:(NSString *)customMsg
-                         timestamp:(NSUInteger)timestamp {
+                         timestamp:(NSUInteger)timestamp
+                           comment:(NSString *)comment {
   self = [super init];
   if (self) {
     if (identifier.length == 0) {
@@ -122,6 +123,7 @@ static const NSUInteger kExpectedTeamIDLength = 10;
     _type = type;
     _customMsg = customMsg;
     _timestamp = timestamp;
+    _comment = comment;
   }
   return self;
 }
@@ -130,7 +132,12 @@ static const NSUInteger kExpectedTeamIDLength = 10;
                              state:(SNTRuleState)state
                               type:(SNTRuleType)type
                          customMsg:(NSString *)customMsg {
-  self = [self initWithIdentifier:identifier state:state type:type customMsg:customMsg timestamp:0];
+  self = [self initWithIdentifier:identifier
+                            state:state
+                             type:type
+                        customMsg:customMsg
+                        timestamp:0
+                          comment:nil];
   // Initialize timestamp to current time if rule is transitive.
   if (self && state == SNTRuleStateAllowTransitive) {
     [self resetTimestamp];
@@ -222,6 +229,7 @@ static const NSUInteger kExpectedTeamIDLength = 10;
   ENCODE(self.customMsg, @"custommsg");
   ENCODE(self.customURL, @"customurl");
   ENCODE(@(self.timestamp), @"timestamp");
+  ENCODE(self.comment, @"comment");
 }
 
 - (instancetype)initWithCoder:(NSCoder *)decoder {
@@ -233,6 +241,7 @@ static const NSUInteger kExpectedTeamIDLength = 10;
     _customMsg = DECODE(NSString, @"custommsg");
     _customURL = DECODE(NSString, @"customurl");
     _timestamp = [DECODE(NSNumber, @"timestamp") unsignedIntegerValue];
+    _comment = DECODE(NSString, @"comment");
   }
   return self;
 }
@@ -270,7 +279,8 @@ static const NSUInteger kExpectedTeamIDLength = 10;
     kRulePolicy : [self ruleStateToPolicyString:self.state],
     kRuleType : [self ruleTypeToString:self.type],
     kRuleCustomMsg : self.customMsg ?: @"",
-    kRuleCustomURL : self.customURL ?: @""
+    kRuleCustomURL : self.customURL ?: @"",
+    kRuleComment : self.comment ?: @"",
   };
 }
 

Source/common/SNTRuleTest.m
@@ -233,6 +233,7 @@
     @"rule_type" : @"TEAMID",
     @"custom_msg" : @"A custom block message",
     @"custom_url" : @"https://example.com",
+    @"comment" : @"",
   };
 
   SNTRule *sut = [[SNTRule alloc] initWithDictionary:expectedTeamID];
@@ -245,6 +246,7 @@
     @"rule_type" : @"BINARY",
     @"custom_msg" : @"",
     @"custom_url" : @"",
+    @"comment" : @"",
   };
 
   sut = [[SNTRule alloc] initWithDictionary:expectedBinary];

Source/common/SNTSyncConstants.h
@@ -132,6 +132,7 @@ extern NSString *const kRuleTypeSigningID;
 extern NSString *const kRuleTypeCDHash;
 extern NSString *const kRuleCustomMsg;
 extern NSString *const kRuleCustomURL;
+extern NSString *const kRuleComment;
 extern NSString *const kCursor;
 
 extern NSString *const kBackoffInterval;

Source/common/SNTSyncConstants.m
@@ -133,6 +133,7 @@ NSString *const kRuleTypeSigningID = @"SIGNINGID";
 NSString *const kRuleTypeCDHash = @"CDHASH";
 NSString *const kRuleCustomMsg = @"custom_msg";
 NSString *const kRuleCustomURL = @"custom_url";
+NSString *const kRuleComment = @"comment";
 NSString *const kCursor = @"cursor";
 
 NSString *const kBackoffInterval = @"backoff";

Source/santactl/Commands/SNTCommandRule.m
@@ -76,7 +76,8 @@ REGISTER_COMMAND_NAME(@"rule")
 #ifdef DEBUG
     @"    --force: allow manual changes even when SyncBaseUrl is set\n"
 #endif
-    @"    --message {message}: custom message\n"
+    @"    --message {message}: custom message to show when binary is blocked\n"
+    @"    --comment {comment}: comment to attach to a new rule\n"
     @"    --clean: when importing rules via JSON clear all non-transitive rules before importing\n"
     @"    --clean-all: when importing rules via JSON clear all rules before importing\n"
     @"\n"
@@ -182,6 +183,11 @@ REGISTER_COMMAND_NAME(@"rule")
         [self printErrorUsageAndExit:@"--message requires an argument"];
       }
       newRule.customMsg = arguments[i];
+    } else if ([arg caseInsensitiveCompare:@"--comment"] == NSOrderedSame) {
+      if (++i > arguments.count - 1) {
+        [self printErrorUsageAndExit:@"--comment requires an argument"];
+      }
+      newRule.comment = arguments[i];
 #ifdef DEBUG
     } else if ([arg caseInsensitiveCompare:@"--force"] == NSOrderedSame) {
       // Don't do anything special.
@@ -271,6 +277,10 @@ REGISTER_COMMAND_NAME(@"rule")
       } else if (cs.platformBinary) {
         newRule.identifier = [NSString stringWithFormat:@"platform:%@", cs.signingID];
       }
+    }
+
+    if (!newRule.comment) {
+      newRule.comment = [NSString stringWithFormat:@"Rule created from %@", path];
     }
   }
 

commit c6ab1b8a90b32d53935d2e11a12fd0c345676b4b

Fix: Database Rule Table Version number (#154)
This PR fixes an issue with the SNTRuleTable's `currentSupportedVersion`
method returning the incorrect value due to the constant it wraps not
being updated.

It makes the `currentSupportedVersion` method part of the interface, so
as to expose for testing and metrics.

A unit test is added to ensure that this constant is updated in the
future.

This bug was introduced in #123 was causing all rules to be dropped
across builds / restarts.

This PR also adds the imports for the BUILD files to satisfy the linter.

Source/common/BUILD
@@ -1,3 +1,4 @@
+load("@com_google_protobuf//bazel:proto_library.bzl", "proto_library")
 load("@rules_cc//cc:defs.bzl", "cc_proto_library")
 load("//:helper.bzl", "santa_unit_test")
 
@@ -166,8 +167,8 @@ objc_library(
     srcs = ["TelemetryEventMap.mm"],
     hdrs = ["TelemetryEventMap.h"],
     deps = [
-      ":String",
-      "@com_google_absl//absl/container:flat_hash_map",
+        ":String",
+        "@com_google_absl//absl/container:flat_hash_map",
     ],
 )
 

Source/santad/DataLayer/SNTDatabaseTable.h
@@ -40,4 +40,15 @@
 - (void)inDatabase:(void (^)(FMDatabase *db))block;
 - (void)inTransaction:(void (^)(FMDatabase *db, BOOL *rollback))block;
 
+///
+///  Current supported version of the table schema. This should be overriden in
+///  subclasses.
+///
+- (uint32_t)currentSupportedVersion;
+
+///
+/// @return The current version of the table schema.
+///
+- (uint32_t)currentVersion;
+
 @end

Source/santad/DataLayer/SNTDatabaseTable.m
@@ -13,6 +13,7 @@
 ///    limitations under the License.
 
 #import "Source/santad/DataLayer/SNTDatabaseTable.h"
+#include <stdint.h>
 
 #include <sqlite3.h>
 
@@ -73,6 +74,15 @@
 - (uint32_t)currentSupportedVersion {
   [self doesNotRecognizeSelector:_cmd];
   return 0;
+}
+
+- (uint32_t)currentVersion {
+  __block uint32_t curVersion = 0;
+  [self.dbQ inDatabase:^(FMDatabase *db) {
+    curVersion = [db userVersion];
+  }];
+
+  return curVersion;
 }
 
 /// Called at the end of initialization to ensure the table in the

Source/santad/DataLayer/SNTRuleTable.m
@@ -26,7 +26,7 @@
 #import "Source/common/SNTRule.h"
 #import "Source/common/SigningIDHelpers.h"
 
-static const uint32_t kRuleTableCurrentVersion = 7;
+static const uint32_t kRuleTableCurrentVersion = 8;
 
 // TODO(nguyenphillip): this should be configurable.
 // How many rules must be in database before we start trying to remove transitive rules.

Source/santad/Logs/EndpointSecurity/Writers/FSSpool/BUILD
@@ -1,3 +1,4 @@
+load("@com_google_protobuf//bazel:proto_library.bzl", "proto_library")
 load("@rules_cc//cc:defs.bzl", "cc_proto_library")
 load("//:helper.bzl", "santa_unit_test")
 

Source/santad/ProcessTree/BUILD
@@ -1,3 +1,4 @@
+load("@com_google_protobuf//bazel:proto_library.bzl", "proto_library")
 load("@rules_cc//cc:defs.bzl", "cc_proto_library")
 load("//:helper.bzl", "santa_unit_test")
 

Source/santad/DataLayer/SNTRuleTableTest.m
@@ -16,6 +16,7 @@
 #import <MOLCodesignChecker/MOLCodesignChecker.h>
 #import <OCMock/OCMock.h>
 #import <XCTest/XCTest.h>
+#include <stdint.h>
 
 #import "Source/common/SNTCachedDecision.h"
 #import "Source/common/SNTConfigurator.h"
@@ -531,6 +532,15 @@
   NSString *teamID = [signingID componentsSeparatedByString:@":"][0];
   XCTAssertEqualObjects(signingID, cd.signingID, @"signing IDs should match");
   XCTAssertEqualObjects(teamID, cd.teamID, @"team IDs should match");
+}
+
+// This test ensures that we bump the constant on updates to the rule table
+// schema.
+- (void)testConstantVersionIsUpdated {
+  uint32_t expectedValue = 8;
+  uint32_t constantVersion = [self.sut currentSupportedVersion];
+  XCTAssertEqual(expectedValue, [self.sut currentVersion], @"currentSchemaVersion should be 8");
+  XCTAssertEqual(expectedValue, constantVersion, @"currentSupportedVersion should be 8");
 }
 
 @end