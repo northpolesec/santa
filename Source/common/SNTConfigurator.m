8a991168f60046371f8207ea6bd31ded40d2a53c
Matt W <436037+mlw@users.noreply.github.com>
2025-01-17 09:59:24 -0500

    Add back code to watch changes to our defaults domain (#214)
    Removed inadvertently here:
    https://github.com/northpolesec/santa/pull/200

--- Source/common/SNTConfigurator.m
+++ Source/common/SNTConfigurator.m
@@ -37,6 +37,8 @@ static NSArray<NSString *> *EnsureArrayOfStrings(id obj) {
 }
 
 @interface SNTConfigurator ()
+@property(readonly, nonatomic) NSUserDefaults *defaults;
+
 /// Keys and expected value types.
 @property(readonly, nonatomic) NSDictionary *syncServerKeyTypes;
 @property(readonly, nonatomic) NSDictionary *forcedConfigKeyTypes;
@@ -295,6 +297,10 @@ static NSString *const kSyncTypeRequired = @"SyncTypeRequired";
 
     _syncStateFilePath = syncStateFilePath;
     _syncStateAccessAuthorizerBlock = syncStateAccessAuthorizer;
+
+    // This is used to keep KVO on changes, but we use `CFPreferences*` for reading.
+    _defaults = [NSUserDefaults standardUserDefaults];
+    [_defaults addSuiteNamed:@"com.northpolesec.santa"];
 
     _configState = [self readForcedConfig];
     [self cacheStaticRules];
