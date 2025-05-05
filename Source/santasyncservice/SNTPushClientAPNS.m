9d7b7c50d70a0780b6afd42e42e36506af92fa6b
Russell Hancox <russell@hancox.us>
2025-05-05 10:34:48 -0400

    Fix lint

--- Source/santasyncservice/SNTPushClientAPNS.m
+++ Source/santasyncservice/SNTPushClientAPNS.m
@@ -64,10 +64,10 @@
 
   // The APNS payload is purposefully _very_ simple given the way APNS handles message re-delivery.
   // The payload dictionary contains a single key "S" with a value of either 0 or 1. If the value is
-  // 0 then the sync should happen immediately. If the value is 1 then the sync should happen after a
-  // random delay between now and the global rule sync deadline. This allows sync servers implementing
-  // APNS support to smear syncs of a large number of hosts to avoid a thundering herd, while still
-  // reaching resolution within a reasonable time period.
+  // 0 then the sync should happen immediately. If the value is 1 then the sync should happen after
+  // a random delay between now and the global rule sync deadline. This allows sync servers
+  // implementing APNS support to smear syncs of a large number of hosts to avoid a thundering herd,
+  // while still reaching resolution within a reasonable time period.
   id syncType = message[@"S"];
   if ([syncType isKindOfClass:[NSNumber class]] && [(NSNumber *)syncType integerValue] == 1) {
     uint32_t delaySeconds = arc4random_uniform((uint32_t)self.globalRuleSyncDeadline);
