2216e5adadc24173b51d4b25461433e7aadeb11c
Matt W <436037+mlw@users.noreply.github.com>
2025-07-30 15:51:49 -0400

    Add nullability to stored events. Fix unwrap of missing data.

--- Source/common/SNTStoredExecutionEvent.h
+++ Source/common/SNTStoredExecutionEvent.h
@@ -23,13 +23,13 @@
 /// Represents an execution event stored in the events database.
 @interface SNTStoredExecutionEvent : SNTStoredEvent <NSSecureCoding>
 
-- (instancetype)initWithFileInfo:(SNTFileInfo *)fileInfo;
+- (nullable instancetype)initWithFileInfo:(nullable SNTFileInfo *)fileInfo;
 
 /// The SHA-256 of the executed file.
-@property NSString *fileSHA256;
+@property(nullable) NSString *fileSHA256;
 
 /// The full path of the executed file.
-@property NSString *filePath;
+@property(nullable) NSString *filePath;
 
 /// Set to YES if the event is a part of a bundle. When an event is passed to SantaGUI this propery
 /// will be used as an indicator to to kick off bundle hashing as necessary. Default value is NO.
@@ -37,45 +37,45 @@
 
 /// If the executed file was part of a bundle, this is the calculated hash of all the nested
 /// executables within the bundle.
-@property NSString *fileBundleHash;
+@property(nullable) NSString *fileBundleHash;
 
 /// If the executed file was part of a bundle, this is the time in ms it took to hash the bundle.
-@property NSNumber *fileBundleHashMilliseconds;
+@property(nullable) NSNumber *fileBundleHashMilliseconds;
 
 /// If the executed file was part of a bundle, this is the total count of related mach-o binaries.
-@property NSNumber *fileBundleBinaryCount;
+@property(nullable) NSNumber *fileBundleBinaryCount;
 
 /// If the executed file was part of the bundle, this is the CFBundleDisplayName, if it exists
 /// or the CFBundleName if not.
-@property NSString *fileBundleName;
+@property(nullable) NSString *fileBundleName;
 
 /// If the executed file was part of the bundle, this is the path to the bundle.
-@property NSString *fileBundlePath;
+@property(nullable) NSString *fileBundlePath;
 
 /// The relative path to the bundle's main executable.
-@property NSString *fileBundleExecutableRelPath;
+@property(nullable) NSString *fileBundleExecutableRelPath;
 
 /// If the executed file was part of the bundle, this is the CFBundleID.
-@property NSString *fileBundleID;
+@property(nullable) NSString *fileBundleID;
 
 /// If the executed file was part of the bundle, this is the CFBundleVersion.
-@property NSString *fileBundleVersion;
+@property(nullable) NSString *fileBundleVersion;
 
 /// If the executed file was part of the bundle, this is the CFBundleShortVersionString.
-@property NSString *fileBundleVersionString;
+@property(nullable) NSString *fileBundleVersionString;
 
 /// If the executed file was signed, this is an NSArray of MOLCertificate's
 /// representing the signing chain.
-@property NSArray<MOLCertificate *> *signingChain;
+@property(nullable) NSArray<MOLCertificate *> *signingChain;
 
 /// If the executed file was signed, this is the Team ID if present in the signature information.
-@property NSString *teamID;
+@property(nullable) NSString *teamID;
 
 /// If the executed file was signed, this is the Signing ID if present in the signature information.
-@property NSString *signingID;
+@property(nullable) NSString *signingID;
 
 /// If the executed file was signed, this is the CDHash of the binary.
-@property NSString *cdhash;
+@property(nullable) NSString *cdhash;
 
 /// Codesigning flags for the process (from `<Kernel/kern/cs_blobs.h>`)
 @property uint32_t codesigningFlags;
@@ -84,43 +84,43 @@
 @property SNTSigningStatus signingStatus;
 
 /// The user who executed the binary.
-@property NSString *executingUser;
+@property(nullable) NSString *executingUser;
 
 /// The decision santad returned.
 @property SNTEventState decision;
 
 /// NSArray of logged in users when the decision was made.
-@property NSArray *loggedInUsers;
+@property(nullable) NSArray *loggedInUsers;
 
 /// NSArray of sessions when the decision was made (e.g. nobody@console, nobody@ttys000).
-@property NSArray *currentSessions;
+@property(nullable) NSArray *currentSessions;
 
 /// The process ID of the binary being executed.
-@property NSNumber *pid;
+@property(nullable) NSNumber *pid;
 
 /// The parent process ID of the binary being executed.
-@property NSNumber *ppid;
+@property(nullable) NSNumber *ppid;
 
 /// The name of the parent process.
-@property NSString *parentName;
+@property(nullable) NSString *parentName;
 
 /// Quarantine data about the executed file, if any.
-@property NSString *quarantineDataURL;
-@property NSString *quarantineRefererURL;
-@property NSDate *quarantineTimestamp;
-@property NSString *quarantineAgentBundleID;
+@property(nullable) NSString *quarantineDataURL;
+@property(nullable) NSString *quarantineRefererURL;
+@property(nullable) NSDate *quarantineTimestamp;
+@property(nullable) NSString *quarantineAgentBundleID;
 
 /// A generated string representing the publisher based on the signingChain
-@property(readonly) NSString *publisherInfo;
+@property(readonly, nullable) NSString *publisherInfo;
 
 /// Return an array of the underlying SecCertificateRef's of the signingChain
 ///
 /// WARNING: If the refs need to be used for a long time be careful to properly
 /// CFRetain/CFRelease the returned items.
-@property(readonly) NSArray *signingChainCertRefs;
+@property(readonly, nullable) NSArray *signingChainCertRefs;
 
 /// If the executed file was entitled, this is the set of key/value pairs of entitlements
-@property NSDictionary *entitlements;
+@property(nullable) NSDictionary *entitlements;
 
 /// Whether or not the set of entitlements were filtered (e.g. due to configuration)
 @property BOOL entitlementsFiltered;
@@ -128,11 +128,11 @@
 /// The timestamp of when the binary was signed. This timestamp is the secure
 /// timestamp that was certified by Apple's timestamp authority service and can
 /// be trusted.
-@property NSDate *secureSigningTime;
+@property(nullable) NSDate *secureSigningTime;
 
 /// The timestamp of when the binary was signed. This timestamp is the insecure
 /// timestamp provided by the developer during signing. It has not been validated
 /// and could be spoofed.
-@property NSDate *signingTime;
+@property(nullable) NSDate *signingTime;
 
 @end
