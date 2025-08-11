e2bffc6cc8cb8350f6bb4b40f736b388c380d041
Matt W <436037+mlw@users.noreply.github.com>
2025-08-11 15:26:20 -0400

    Support XXH3 64bit

--- Source/common/SNTXxhashTest.mm
+++ Source/common/SNTXxhashTest.mm
@@ -25,8 +25,39 @@
 
 @implementation SNTXxhashTest
 
-- (void)testXxhash {
-  santa::Xxhash state;
+- (void)testXxhash64 {
+  santa::Xxhash64 state;
+
+  state.Update("hello", 5);
+  XCTAssertCppStringEqual(state.Digest(), "9555e8555c62dcfd");
+
+  state.Update("world", 5);
+  XCTAssertCppStringEqual(state.Digest(), "ffdcce9d0e1f7d46");
+}
+
+- (void)testXxhashCopyState64 {
+  santa::Xxhash64 state;
+  state.Update("hello", 5);
+  XCTAssertCppStringEqual(state.Digest(), "9555e8555c62dcfd");
+
+  // Check that the copying of existing state works.
+  santa::Xxhash64 state2(state);
+  XCTAssertCppStringEqual(state2.Digest(), "9555e8555c62dcfd");
+  state2.Update("bye", 3);
+  XCTAssertCppStringEqual(state2.Digest(), "0a20389200c43514");
+
+  // Ensure that the original state is not modified
+  XCTAssertCppStringEqual(state.Digest(), "9555e8555c62dcfd");
+
+  // Ensure that a second copy has identical results to the first.
+  santa::Xxhash64 state3(state);
+  XCTAssertCppStringEqual(state3.Digest(), "9555e8555c62dcfd");
+  state3.Update("bye", 3);
+  XCTAssertCppStringEqual(state3.Digest(), "0a20389200c43514");
+}
+
+- (void)testXxhash128 {
+  santa::Xxhash128 state;
 
   state.Update("hello", 5);
   XCTAssertCppStringEqual(state.Digest(), "b5e9c1ad071b3e7fc779cfaa5e523818");
@@ -35,13 +66,13 @@
   XCTAssertCppStringEqual(state.Digest(), "ddf7ff67be2b60f50f178df33653476f");
 }
 
-- (void)testXxhashCopyState {
-  santa::Xxhash state;
+- (void)testXxhashCopyState128 {
+  santa::Xxhash128 state;
   state.Update("hello", 5);
   XCTAssertCppStringEqual(state.Digest(), "b5e9c1ad071b3e7fc779cfaa5e523818");
 
   // Check that the copying of existing state works.
-  santa::Xxhash state2(state);
+  santa::Xxhash128 state2(state);
   XCTAssertCppStringEqual(state2.Digest(), "b5e9c1ad071b3e7fc779cfaa5e523818");
   state2.Update("bye", 3);
   XCTAssertCppStringEqual(state2.Digest(), "a02b1c0f54414e59c9cb785db0a4cfc8");
@@ -50,7 +81,7 @@
   XCTAssertCppStringEqual(state.Digest(), "b5e9c1ad071b3e7fc779cfaa5e523818");
 
   // Ensure that a second copy has identical results to the first.
-  santa::Xxhash state3(state);
+  santa::Xxhash128 state3(state);
   XCTAssertCppStringEqual(state3.Digest(), "b5e9c1ad071b3e7fc779cfaa5e523818");
   state3.Update("bye", 3);
   XCTAssertCppStringEqual(state3.Digest(), "a02b1c0f54414e59c9cb785db0a4cfc8");
