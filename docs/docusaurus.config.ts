9ae9d05dfcc9075e3ec7277290b86e65112c8628
Russell Hancox <russellhancox@users.noreply.github.com>
2025-08-13 13:26:00 -0700

    docs: Add trailingSlash config (#532)

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -14,6 +14,8 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   baseUrl: "/",
 
+  trailingSlash: true,
+
   // GitHub pages deployment config.
   organizationName: "northpolesec",
   projectName: "santa",
