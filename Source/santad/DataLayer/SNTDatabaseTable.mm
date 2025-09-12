northpolesec/santa

commit 05a606c6cfbb5bdeb6662384b22f431542bda335

Support FAA block events in the sync EventUpload phase (#569)
This PR adds support for storing FAA block events into the sync
protocol.

This also sets up the Santa repo to be able to house one-off utilities.
This first one, SNTStoredEventGenerator is used to help create test
input files.

---------

Signed-off-by: Matt W <436037+mlw@users.noreply.github.com>
Co-authored-by: Russell Hancox <russellhancox@users.noreply.github.com>

Source/santad/DataLayer/SNTEventTable.mm
@@ -21,7 +21,7 @@
 #import "Source/common/SNTStoredExecutionEvent.h"
 #import "Source/common/SNTStoredFileAccessEvent.h"
 
-static const uint32_t kEventTableCurrentVersion = 4;
+static const uint32_t kEventTableCurrentVersion = 5;
 
 @implementation SNTEventTable
 
@@ -73,6 +73,15 @@ static const uint32_t kEventTableCurrentVersion = 4;
     newVersion = 4;
   }
 
+  if (version < 5) {
+    // Rename the filesha256 column to uniqueid because different stored event types
+    // contain different content for determining uniqueness.
+    [db executeUpdate:@"DROP INDEX filesha256"];
+    [db executeUpdate:@"ALTER TABLE events RENAME COLUMN filesha256 TO uniqueid"];
+    [db executeUpdate:@"CREATE UNIQUE INDEX uniqueid ON events (uniqueid)"];
+    newVersion = 4;
+  }
+
   return newVersion;
 }
 
@@ -85,12 +94,12 @@ static const uint32_t kEventTableCurrentVersion = 4;
 - (BOOL)isValidStoredEvent:(SNTStoredEvent *)event {
   if ([event isKindOfClass:[SNTStoredExecutionEvent class]]) {
     SNTStoredExecutionEvent *se = (SNTStoredExecutionEvent *)event;
-    return se.idx && [[se hashForEvent] length] && se.filePath.length && se.occurrenceDate &&
+    return se.idx && [[se uniqueID] length] && se.filePath.length && se.occurrenceDate &&
            se.decision;
   } else if ([event isKindOfClass:[SNTStoredFileAccessEvent class]]) {
     SNTStoredFileAccessEvent *se = (SNTStoredFileAccessEvent *)event;
     return se.idx && se.ruleVersion.length && se.ruleName.length && se.accessedPath.length &&
-           se.process.filePath.length && [[se hashForEvent] length];
+           se.process.filePath.length && [[se uniqueID] length];
   } else {
     return NO;
   }
@@ -115,10 +124,10 @@ static const uint32_t kEventTableCurrentVersion = 4;
   [self inTransaction:^(FMDatabase *db, BOOL *rollback) {
     [eventsData
         enumerateKeysAndObjectsUsingBlock:^(NSData *eventData, SNTStoredEvent *event, BOOL *stop) {
-          success = [db executeUpdate:@"INSERT INTO 'events' (idx, filesha256, eventdata) "
+          success = [db executeUpdate:@"INSERT INTO 'events' (idx, uniqueid, eventdata) "
                                       @"VALUES (?, ?, ?) "
-                                      @"ON CONFLICT(filesha256) DO NOTHING",
-                                      event.idx, [event hashForEvent], eventData];
+                                      @"ON CONFLICT(uniqueid) DO NOTHING",
+                                      event.idx, [event uniqueID], eventData];
           if (!success) *stop = YES;
         }];
   }];
@@ -162,9 +171,11 @@ static const uint32_t kEventTableCurrentVersion = 4;
   if (!eventData) return nil;
 
   NSError *err;
-  SNTStoredEvent *event = [NSKeyedUnarchiver unarchivedObjectOfClass:[SNTStoredExecutionEvent class]
-                                                            fromData:eventData
-                                                               error:&err];
+  SNTStoredEvent *event = [NSKeyedUnarchiver
+      unarchivedObjectOfClasses:[NSSet setWithObjects:[SNTStoredExecutionEvent class],
+                                                      [SNTStoredFileAccessEvent class], nil]
+                       fromData:eventData
+                          error:&err];
 
   if (event && !err && [self isValidStoredEvent:event]) {
     return event;

BUILD
@@ -241,6 +241,10 @@ test_suite(
         "//Source/santad:unit_tests",
         "//Source/santametricservice:unit_tests",
         "//Source/santasyncservice:unit_tests",
+
+        # Trigger a build of one-off utils. This won't run anything but helps ensure
+        # code changes don't break them.
+        "//Testing/OneOffs:BuildOnly",
     ],
 )
 

MODULE.bazel
@@ -17,7 +17,7 @@ bazel_dep(name = "zstd", version = "1.5.7")
 bazel_dep(name = "protos", version = "1.0.1", repo_name = "northpolesec_protos")
 git_override(
     module_name = "protos",
-    commit = "ad45e21f3c36e81f20837195b2eab8bca91c7e9e",
+    commit = "bef56394f92baa4d69f0b18e568339f9d428fa4b",
     remote = "https://github.com/northpolesec/protos",
 )
 

Source/common/SNTStoredEvent.h
@@ -24,8 +24,8 @@
 /// An index for this event, randomly generated during initialization.
 @property(nonnull) NSNumber *idx;
 
-// Subclasses are required to define: `hashForEvent`.
+// Subclasses are required to define: `uniqueID`.
 // The base class implementation will throw an exception.
-- (nullable NSString *)hashForEvent;
+- (nullable NSString *)uniqueID;
 
 @end

Source/common/SNTStoredEvent.mm
@@ -60,7 +60,7 @@
   return result;
 }
 
-- (NSString *)hashForEvent {
+- (NSString *)uniqueID {
   [self doesNotRecognizeSelector:_cmd];
   return nil;
 }

Source/common/SNTStoredEventTest.mm
@@ -24,19 +24,68 @@
 
 @implementation StoredEventTest
 
-- (void)testHashForEvent {
+- (void)testUniqueID {
   // The base class should throw
   SNTStoredEvent *baseEvent = [[SNTStoredEvent alloc] init];
-  XCTAssertThrows([baseEvent hashForEvent]);
+  XCTAssertThrows([baseEvent uniqueID]);
 
   // Derived classes should not throw
   SNTStoredExecutionEvent *execEvent = [[SNTStoredExecutionEvent alloc] init];
   execEvent.fileSHA256 = @"foo";
-  XCTAssertEqualObjects([execEvent hashForEvent], @"foo");
+  XCTAssertEqualObjects([execEvent uniqueID], @"foo");
+
+  SNTStoredFileAccessEvent *faaEvent = [[SNTStoredFileAccessEvent alloc] init];
+  faaEvent.ruleName = @"MyRule";
+  faaEvent.ruleVersion = @"MyVersion";
+  faaEvent.accessedPath = @"/not/included";
+  faaEvent.process.fileSHA256 = @"bar";
+  XCTAssertEqualObjects([faaEvent uniqueID], @"MyRule|MyVersion|bar");
+}
+
+- (void)testEncodeDecode {
+  SNTStoredExecutionEvent *execEvent = [[SNTStoredExecutionEvent alloc] init];
+  execEvent.fileSHA256 = @"foo";
 
   SNTStoredFileAccessEvent *faaEvent = [[SNTStoredFileAccessEvent alloc] init];
   faaEvent.process.fileSHA256 = @"bar";
-  XCTAssertEqualObjects([faaEvent hashForEvent], @"bar");
+
+  faaEvent.process.parent = [[SNTStoredFileAccessProcess alloc] init];
+  faaEvent.process.parent.pid = @(123);
+
+  NSData *archivedExecEvent = [NSKeyedArchiver archivedDataWithRootObject:execEvent
+                                                    requiringSecureCoding:YES
+                                                                    error:nil];
+  NSData *archivedFaaEvent = [NSKeyedArchiver archivedDataWithRootObject:faaEvent
+                                                   requiringSecureCoding:YES
+                                                                   error:nil];
+
+  XCTAssertNotNil(archivedExecEvent);
+  XCTAssertNotNil(archivedFaaEvent);
+
+  SNTStoredEvent *unarchivedExecEvent = [NSKeyedUnarchiver
+      unarchivedObjectOfClasses:[NSSet setWithObjects:[SNTStoredExecutionEvent class],
+                                                      [SNTStoredFileAccessEvent class], nil]
+                       fromData:archivedExecEvent
+                          error:nil];
+
+  XCTAssertNotNil(unarchivedExecEvent);
+  XCTAssertTrue([unarchivedExecEvent isKindOfClass:[SNTStoredExecutionEvent class]]);
+  XCTAssertEqualObjects(((SNTStoredExecutionEvent *)unarchivedExecEvent).fileSHA256, @"foo");
+
+  SNTStoredEvent *unarchivedFaaEvent = [NSKeyedUnarchiver
+      unarchivedObjectOfClasses:[NSSet setWithObjects:[SNTStoredExecutionEvent class],
+                                                      [SNTStoredFileAccessEvent class], nil]
+                       fromData:archivedFaaEvent
+                          error:nil];
+
+  XCTAssertNotNil(unarchivedFaaEvent);
+  XCTAssertTrue([unarchivedFaaEvent isKindOfClass:[SNTStoredFileAccessEvent class]]);
+  XCTAssertEqualObjects(((SNTStoredFileAccessEvent *)unarchivedFaaEvent).process.fileSHA256,
+                        @"bar");
+  XCTAssertNotNil(((SNTStoredFileAccessEvent *)unarchivedFaaEvent).process.parent);
+  XCTAssertEqualObjects(((SNTStoredFileAccessEvent *)unarchivedFaaEvent).process.parent.pid,
+                        @(123));
+  XCTAssertNil(((SNTStoredFileAccessEvent *)unarchivedFaaEvent).process.parent.parent);
 }
 
 @end

Source/common/SNTStoredExecutionEvent.mm
@@ -167,7 +167,7 @@
   return CertificateChain(self.signingChain);
 }
 
-- (NSString *)hashForEvent {
+- (NSString *)uniqueID {
   return self.fileSHA256;
 }
 

Source/common/SNTStoredFileAccessEvent.mm
@@ -50,12 +50,14 @@
 }
 
 - (NSString *)description {
-  return [NSString stringWithFormat:@"SNTStoredFileAccessEvent: Accessed: %@, By: %@",
+  return [NSString stringWithFormat:@"SNTStoredFileAccessEvent[%@]: Accessed: %@, By: %@", self.idx,
                                     self.accessedPath, self.process];
 }
 
-- (NSString *)hashForEvent {
-  return _process.fileSHA256;
+- (NSString *)uniqueID {
+  // NB: Not using `accessedPath` as part of the uniqe ID to prevent a noisy
+  // rule from generating a large number of events to upload.
+  return [NSString stringWithFormat:@"%@|%@|%@", _ruleName, _ruleVersion, _process.fileSHA256];
 }
 
 @end

commit 48cab03b4a9732ffd29474fdd9d5987e681b1541

Fix version issue with last events table upgrade (#578)
Bug was not part of a release, introduced the other day in #569 -
https://github.com/northpolesec/santa/pull/569/files#diff-9236068babaeecd117a8944c96d84288c9896248a83ef71155d64ae1a543484fR82

Source/santad/DataLayer/SNTDatabaseTable.mm
@@ -92,6 +92,9 @@
   [self inTransaction:^(FMDatabase *db, BOOL *rollback) {
     uint32_t currentVersion = [db userVersion];
     uint32_t newVersion = [self initializeDatabase:db fromVersion:currentVersion];
+    // For debug builds, assert that the returned new version matches what is expected.
+    // Version 0 is allowed since that means no upgrade was necessary.
+    assert(newVersion == 0 || newVersion == [self currentSupportedVersion]);
     if (newVersion < 1) return;
 
     LOGI(@"Updated %@ from version %d to %d", [self className], currentVersion, newVersion);

Source/santad/DataLayer/SNTEventTable.mm
@@ -79,7 +79,7 @@ static const uint32_t kEventTableCurrentVersion = 5;
     [db executeUpdate:@"DROP INDEX filesha256"];
     [db executeUpdate:@"ALTER TABLE events RENAME COLUMN filesha256 TO uniqueid"];
     [db executeUpdate:@"CREATE UNIQUE INDEX uniqueid ON events (uniqueid)"];
-    newVersion = 4;
+    newVersion = 5;
   }
 
   return newVersion;