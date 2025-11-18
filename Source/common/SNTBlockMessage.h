northpolesec/santa

commit 7b1498b74a6d4c00ec4dbb8b140d4d6450f71296

Support EventDetailURL and EventDetailText in the sync protocol (#642)
These two configuration keys can now be set by the sync server if
desired.

Source/common/SNTBlockMessage.h
@@ -53,8 +53,9 @@ NS_ASSUME_NONNULL_BEGIN
 ///  Return a URL generated from the EventDetailURL configuration key
 ///  after replacing templates in the URL with values from the event.
 ///
-+ (nullable NSURL *)eventDetailURLForEvent:(nullable SNTStoredExecutionEvent *)event
-                                 customURL:(nullable NSString *)url;
++ (nullable NSURL *)eventDetailURLForEvent:(SNTStoredExecutionEvent *)event
+                                 customURL:(nullable NSString *)url
+                               fallbackURL:(nullable NSString *)fallbackURL;
 + (nullable NSURL *)eventDetailURLForFileAccessEvent:(nullable SNTStoredFileAccessEvent *)event
                                            customURL:(nullable NSString *)url;
 

Source/common/SNTBlockMessage.mm
@@ -241,12 +241,11 @@ static id ValueOrNull(id value) {
 // The "format strings" in `templateMapping` will be replaced in the URL, if they are present.
 + (NSURL *)eventDetailURLForEvent:(SNTStoredEvent *)event
                         customURL:(NSString *)url
+                      fallbackURL:(NSString *)fallbackURL
                   templateMapping:(NSDictionary *)templateMapping {
-  SNTConfigurator *config = [SNTConfigurator configurator];
-
   NSString *formatStr = url;
   if (!formatStr.length) {
-    formatStr = config.eventDetailURL;
+    formatStr = fallbackURL;
     if (!formatStr.length) {
       return nil;
     }
@@ -265,9 +264,12 @@ static id ValueOrNull(id value) {
   return u;
 }
 
-+ (NSURL *)eventDetailURLForEvent:(SNTStoredExecutionEvent *)event customURL:(NSString *)url {
++ (NSURL *)eventDetailURLForEvent:(SNTStoredExecutionEvent *)event
+                        customURL:(NSString *)url
+                      fallbackURL:(NSString *)fallbackURL {
   return [self eventDetailURLForEvent:event
                             customURL:url
+                          fallbackURL:fallbackURL
                       templateMapping:[self eventDetailTemplateMappingForEvent:event]];
 }
 
@@ -275,6 +277,7 @@ static id ValueOrNull(id value) {
                                   customURL:(NSString *)url {
   return [self eventDetailURLForEvent:event
                             customURL:url
+                          fallbackURL:[[SNTConfigurator configurator] eventDetailURL]
                       templateMapping:[self fileAccessEventDetailTemplateMappingForEvent:event]];
 }
 

Source/common/SNTXPCNotifierInterface.h
@@ -27,7 +27,7 @@
 @protocol SNTNotifierXPC
 - (void)postBlockNotification:(SNTStoredExecutionEvent *)event
             withCustomMessage:(NSString *)message
-                    customURL:(NSString *)url
+                    customURL:(NSURL *)url
                   configState:(SNTConfigState *)configState
                      andReply:(void (^)(BOOL authenticated))reply;
 - (void)postUSBBlockNotification:(SNTDeviceEvent *)event;

Source/gui/SNTBinaryMessageWindowController.h
@@ -28,7 +28,7 @@
 
 - (instancetype)initWithEvent:(SNTStoredExecutionEvent *)event
                     customMsg:(NSString *)message
-                    customURL:(NSString *)url
+                    customURL:(NSURL *)url
                   configState:(SNTConfigState *)configState
                         reply:(void (^)(BOOL authenticated))replyBlock;
 

Source/gui/SNTBinaryMessageWindowController.mm
@@ -33,7 +33,7 @@
 @property(copy) NSString *customMessage;
 
 ///  The custom URL to use for this event
-@property(copy) NSString *customURL;
+@property(copy) NSURL *customURL;
 
 @end
 
@@ -41,7 +41,7 @@
 
 - (instancetype)initWithEvent:(SNTStoredExecutionEvent *)event
                     customMsg:(NSString *)message
-                    customURL:(NSString *)url
+                    customURL:(NSURL *)url
                   configState:(SNTConfigState *)configState
                         reply:(void (^)(BOOL))replyBlock {
   self = [super init];

Source/gui/SNTBinaryMessageWindowView.swift
@@ -34,7 +34,7 @@ import santa_gui_SNTMessageView
     window: NSWindow,
     event: SNTStoredExecutionEvent,
     customMsg: NSString?,
-    customURL: NSString?,
+    customURL: NSURL?,
     configState: SNTConfigState,
     bundleProgress: SNTBundleProgress,
     uiStateCallback: ((TimeInterval) -> Void)?,
@@ -56,7 +56,7 @@ import santa_gui_SNTMessageView
   }
 }
 
-func copyDetailsToClipboard(e: SNTStoredExecutionEvent?, customURL: String?) {
+func copyDetailsToClipboard(e: SNTStoredExecutionEvent?, customURL: URL?) {
   var s = "Santa blocked \((e?.fileBundleName?.isEmpty == false) ? e!.fileBundleName! : "an application")"
   if let publisher = e?.publisherInfo {
     s += "\nPublisher  : \(publisher)"
@@ -76,8 +76,7 @@ func copyDetailsToClipboard(e: SNTStoredExecutionEvent?, customURL: String?) {
   s += "\nSHA-256    : \(e?.fileSHA256 ?? "unknown")"
   s += "\nParent     : \(e?.parentName ?? "") (\(String(format: "%d", e?.ppid?.intValue ?? 0)))"
 
-  let url = SNTBlockMessage.eventDetailURL(for: e, customURL: customURL as String?)
-  s += "\nURL        : \(url?.absoluteString ?? "unknown")"
+  s += "\nURL        : \(customURL?.absoluteString ?? "unknown")"
   s += "\n"
 
   let pasteboard = NSPasteboard.general
@@ -87,7 +86,7 @@ func copyDetailsToClipboard(e: SNTStoredExecutionEvent?, customURL: String?) {
 
 struct MoreDetailsView: View {
   let e: SNTStoredExecutionEvent?
-  let customURL: NSString?
+  let customURL: NSURL?
 
   @Environment(\.presentationMode) var presentationMode
 
@@ -156,7 +155,7 @@ struct MoreDetailsView: View {
 
         HStack {
           CopyDetailsButton(action: {
-            copyDetailsToClipboard(e: e, customURL: customURL as String?)
+            copyDetailsToClipboard(e: e, customURL: customURL as URL?)
           })
 
           Button(action: { presentationMode.wrappedValue.dismiss() }) {
@@ -180,7 +179,7 @@ struct MoreDetailsView: View {
 
 struct SNTBinaryMessageEventView: View {
   let e: SNTStoredExecutionEvent?
-  let customURL: NSString?
+  let customURL: NSURL?
 
   @State private var isShowingDetails = false
 
@@ -226,7 +225,7 @@ struct SNTBinaryMessageEventView: View {
         MoreDetailsButton($isShowingDetails)
 
         CopyDetailsButton(action: {
-          copyDetailsToClipboard(e: e, customURL: customURL as String?)
+          copyDetailsToClipboard(e: e, customURL: customURL as URL?)
         })
       }
 
@@ -239,7 +238,7 @@ struct SNTBinaryMessageWindowView: View {
   let window: NSWindow?
   let event: SNTStoredExecutionEvent?
   let customMsg: NSString?
-  let customURL: NSString?
+  let customURL: NSURL?
   let configState: SNTConfigState
   @StateObject var bundleProgress: SNTBundleProgress
   let uiStateCallback: ((TimeInterval) -> Void)?
@@ -300,7 +299,7 @@ struct SNTBinaryMessageWindowView: View {
           StandaloneButton(action: standAloneButton)
         } else if shouldAddOpenButton() {
           OpenEventButton(
-            customText: c.eventDetailText,
+            customText: configState.eventDetailText,
             disabled: (event?.needsBundleHash ?? false && !bundleProgress.isFinished),
             action: openButton
           )
@@ -339,10 +338,7 @@ struct SNTBinaryMessageWindowView: View {
   }
 
   func shouldAddOpenButton() -> Bool {
-    if c.eventDetailURL?.isEmpty ?? true {
-      return false
-    }
-    return true
+    return customURL?.absoluteString?.isEmpty == false
   }
 
   func openButton() {
@@ -360,9 +356,7 @@ struct SNTBinaryMessageWindowView: View {
 
     window?.close()
 
-    let detailsURL = SNTBlockMessage.eventDetailURL(for: event, customURL: customURL as String?)
-
-    if let url = detailsURL {
+    if let url = customURL as URL? {
       openURL(url)
     }
   }

Source/gui/SNTNotificationManager.mm
@@ -405,7 +405,7 @@ static NSString *const silencedNotificationsKey = @"SilencedNotifications";
 
 - (void)postBlockNotification:(SNTStoredExecutionEvent *)event
             withCustomMessage:(NSString *)message
-                    customURL:(NSString *)url
+                    customURL:(NSURL *)url
                   configState:(SNTConfigState *)configState
                      andReply:(void (^)(BOOL))replyBlock {
   if (!event) {

Source/santad/SNTExecutionController.mm
@@ -454,7 +454,9 @@ static NSString *const kPrinterProxyPostMonterey =
                             @"\033[1mIdentifier:\033[0m %@\n"
                             @"\033[1mParent:    \033[0m %@ (%@)\n\n",
                             se.filePath, se.fileSHA256, se.parentName, se.ppid];
-          NSURL *detailURL = [SNTBlockMessage eventDetailURLForEvent:se customURL:cd.customURL];
+          NSURL *detailURL = [SNTBlockMessage eventDetailURLForEvent:se
+                                                           customURL:cd.customURL
+                                                         fallbackURL:config.eventDetailURL];
           if (detailURL) {
             [msg appendFormat:@"More info:\n%@\n\n", detailURL.absoluteString];
           }
@@ -495,7 +497,9 @@ static NSString *const kPrinterProxyPostMonterey =
         // Let the user know what happened in the GUI.
         [self.notifierQueue addEvent:se
                    withCustomMessage:cd.customMsg
-                           customURL:cd.customURL
+                           customURL:[SNTBlockMessage eventDetailURLForEvent:se
+                                                                   customURL:cd.customURL
+                                                                 fallbackURL:config.eventDetailURL]
                          configState:configState
                             andReply:replyBlock];
       }

commit 705ae18cc886db17d6af577c4ca335c96177f738

Fix issue with bundle hash event URLs (#646)
This fixes an issue caused by #642 (this issue was never part of a
release)

The event detail URL template was expanded too early, prior to the
bundle hash being available. This meant that the link being opened to
the sync server had the main executable hash only and users would only
approve the main executable instead of the full app.

Source/common/SNTBlockMessage.h
@@ -53,9 +53,8 @@ NS_ASSUME_NONNULL_BEGIN
 ///  Return a URL generated from the EventDetailURL configuration key
 ///  after replacing templates in the URL with values from the event.
 ///
-+ (nullable NSURL *)eventDetailURLForEvent:(SNTStoredExecutionEvent *)event
-                                 customURL:(nullable NSString *)url
-                               fallbackURL:(nullable NSString *)fallbackURL;
++ (nullable NSURL *)eventDetailURLForEvent:(nullable SNTStoredExecutionEvent *)event
+                                 customURL:(nullable NSString *)url;
 + (nullable NSURL *)eventDetailURLForFileAccessEvent:(nullable SNTStoredFileAccessEvent *)event
                                            customURL:(nullable NSString *)url;
 

Source/common/SNTBlockMessage.mm
@@ -241,14 +241,10 @@ static id ValueOrNull(id value) {
 // The "format strings" in `templateMapping` will be replaced in the URL, if they are present.
 + (NSURL *)eventDetailURLForEvent:(SNTStoredEvent *)event
                         customURL:(NSString *)url
-                      fallbackURL:(NSString *)fallbackURL
                   templateMapping:(NSDictionary *)templateMapping {
   NSString *formatStr = url;
   if (!formatStr.length) {
-    formatStr = fallbackURL;
-    if (!formatStr.length) {
-      return nil;
-    }
+    return nil;
   }
 
   if ([formatStr isEqualToString:@"null"]) {
@@ -264,21 +260,18 @@ static id ValueOrNull(id value) {
   return u;
 }
 
-+ (NSURL *)eventDetailURLForEvent:(SNTStoredExecutionEvent *)event
-                        customURL:(NSString *)url
-                      fallbackURL:(NSString *)fallbackURL {
++ (NSURL *)eventDetailURLForEvent:(SNTStoredExecutionEvent *)event customURL:(NSString *)url {
   return [self eventDetailURLForEvent:event
                             customURL:url
-                          fallbackURL:fallbackURL
                       templateMapping:[self eventDetailTemplateMappingForEvent:event]];
 }
 
 + (NSURL *)eventDetailURLForFileAccessEvent:(SNTStoredFileAccessEvent *)event
                                   customURL:(NSString *)url {
-  return [self eventDetailURLForEvent:event
-                            customURL:url
-                          fallbackURL:[[SNTConfigurator configurator] eventDetailURL]
-                      templateMapping:[self fileAccessEventDetailTemplateMappingForEvent:event]];
+  return [self
+      eventDetailURLForEvent:event
+                   customURL:(url ?: [[SNTConfigurator configurator] eventDetailURL])templateMapping
+                            :[self fileAccessEventDetailTemplateMappingForEvent:event]];
 }
 
 @end

Source/common/SNTXPCNotifierInterface.h
@@ -27,7 +27,7 @@
 @protocol SNTNotifierXPC
 - (void)postBlockNotification:(SNTStoredExecutionEvent *)event
             withCustomMessage:(NSString *)message
-                    customURL:(NSURL *)url
+                    customURL:(NSString *)url
                   configState:(SNTConfigState *)configState
                      andReply:(void (^)(BOOL authenticated))reply;
 - (void)postUSBBlockNotification:(SNTDeviceEvent *)event;

Source/gui/SNTBinaryMessageWindowController.h
@@ -28,7 +28,7 @@
 
 - (instancetype)initWithEvent:(SNTStoredExecutionEvent *)event
                     customMsg:(NSString *)message
-                    customURL:(NSURL *)url
+                    customURL:(NSString *)url
                   configState:(SNTConfigState *)configState
                         reply:(void (^)(BOOL authenticated))replyBlock;
 

Source/gui/SNTBinaryMessageWindowController.mm
@@ -33,7 +33,7 @@
 @property(copy) NSString *customMessage;
 
 ///  The custom URL to use for this event
-@property(copy) NSURL *customURL;
+@property(copy) NSString *customURL;
 
 @end
 
@@ -41,7 +41,7 @@
 
 - (instancetype)initWithEvent:(SNTStoredExecutionEvent *)event
                     customMsg:(NSString *)message
-                    customURL:(NSURL *)url
+                    customURL:(NSString *)url
                   configState:(SNTConfigState *)configState
                         reply:(void (^)(BOOL))replyBlock {
   self = [super init];

Source/gui/SNTBinaryMessageWindowView.swift
@@ -34,7 +34,7 @@ import santa_gui_SNTMessageView
     window: NSWindow,
     event: SNTStoredExecutionEvent,
     customMsg: NSString?,
-    customURL: NSURL?,
+    customURL: NSString?,
     configState: SNTConfigState,
     bundleProgress: SNTBundleProgress,
     uiStateCallback: ((TimeInterval) -> Void)?,
@@ -56,7 +56,7 @@ import santa_gui_SNTMessageView
   }
 }
 
-func copyDetailsToClipboard(e: SNTStoredExecutionEvent?, customURL: URL?) {
+func copyDetailsToClipboard(e: SNTStoredExecutionEvent?, customURL: String?) {
   var s = "Santa blocked \((e?.fileBundleName?.isEmpty == false) ? e!.fileBundleName! : "an application")"
   if let publisher = e?.publisherInfo {
     s += "\nPublisher  : \(publisher)"
@@ -76,7 +76,8 @@ func copyDetailsToClipboard(e: SNTStoredExecutionEvent?, customURL: URL?) {
   s += "\nSHA-256    : \(e?.fileSHA256 ?? "unknown")"
   s += "\nParent     : \(e?.parentName ?? "") (\(String(format: "%d", e?.ppid?.intValue ?? 0)))"
 
-  s += "\nURL        : \(customURL?.absoluteString ?? "unknown")"
+  let url = SNTBlockMessage.eventDetailURL(for: e, customURL: customURL as String?)
+  s += "\nURL : \(url?.absoluteString ?? "unknown")"
   s += "\n"
 
   let pasteboard = NSPasteboard.general
@@ -86,7 +87,7 @@ func copyDetailsToClipboard(e: SNTStoredExecutionEvent?, customURL: URL?) {
 
 struct MoreDetailsView: View {
   let e: SNTStoredExecutionEvent?
-  let customURL: NSURL?
+  let customURL: NSString?
 
   @Environment(\.presentationMode) var presentationMode
 
@@ -155,7 +156,7 @@ struct MoreDetailsView: View {
 
         HStack {
           CopyDetailsButton(action: {
-            copyDetailsToClipboard(e: e, customURL: customURL as URL?)
+            copyDetailsToClipboard(e: e, customURL: customURL as String?)
           })
 
           Button(action: { presentationMode.wrappedValue.dismiss() }) {
@@ -179,7 +180,7 @@ struct MoreDetailsView: View {
 
 struct SNTBinaryMessageEventView: View {
   let e: SNTStoredExecutionEvent?
-  let customURL: NSURL?
+  let customURL: NSString?
 
   @State private var isShowingDetails = false
 
@@ -225,7 +226,7 @@ struct SNTBinaryMessageEventView: View {
         MoreDetailsButton($isShowingDetails)
 
         CopyDetailsButton(action: {
-          copyDetailsToClipboard(e: e, customURL: customURL as URL?)
+          copyDetailsToClipboard(e: e, customURL: customURL as String?)
         })
       }
 
@@ -238,7 +239,7 @@ struct SNTBinaryMessageWindowView: View {
   let window: NSWindow?
   let event: SNTStoredExecutionEvent?
   let customMsg: NSString?
-  let customURL: NSURL?
+  let customURL: NSString?
   let configState: SNTConfigState
   @StateObject var bundleProgress: SNTBundleProgress
   let uiStateCallback: ((TimeInterval) -> Void)?
@@ -338,7 +339,7 @@ struct SNTBinaryMessageWindowView: View {
   }
 
   func shouldAddOpenButton() -> Bool {
-    return customURL?.absoluteString?.isEmpty == false
+    return customURL?.length ?? 0 > 0
   }
 
   func openButton() {
@@ -356,7 +357,7 @@ struct SNTBinaryMessageWindowView: View {
 
     window?.close()
 
-    if let url = customURL as URL? {
+    if let url = SNTBlockMessage.eventDetailURL(for: event, customURL: customURL as String?) {
       openURL(url)
     }
   }

Source/gui/SNTNotificationManager.mm
@@ -405,7 +405,7 @@ static NSString *const silencedNotificationsKey = @"SilencedNotifications";
 
 - (void)postBlockNotification:(SNTStoredExecutionEvent *)event
             withCustomMessage:(NSString *)message
-                    customURL:(NSURL *)url
+                    customURL:(NSString *)url
                   configState:(SNTConfigState *)configState
                      andReply:(void (^)(BOOL))replyBlock {
   if (!event) {

Source/santad/SNTExecutionController.mm
@@ -454,9 +454,9 @@ static NSString *const kPrinterProxyPostMonterey =
                             @"\033[1mIdentifier:\033[0m %@\n"
                             @"\033[1mParent:    \033[0m %@ (%@)\n\n",
                             se.filePath, se.fileSHA256, se.parentName, se.ppid];
-          NSURL *detailURL = [SNTBlockMessage eventDetailURLForEvent:se
-                                                           customURL:cd.customURL
-                                                         fallbackURL:config.eventDetailURL];
+          NSURL *detailURL =
+              [SNTBlockMessage eventDetailURLForEvent:se
+                                            customURL:(cd.customURL ?: config.eventDetailURL)];
           if (detailURL) {
             [msg appendFormat:@"More info:\n%@\n\n", detailURL.absoluteString];
           }
@@ -497,9 +497,7 @@ static NSString *const kPrinterProxyPostMonterey =
         // Let the user know what happened in the GUI.
         [self.notifierQueue addEvent:se
                    withCustomMessage:cd.customMsg
-                           customURL:[SNTBlockMessage eventDetailURLForEvent:se
-                                                                   customURL:cd.customURL
-                                                                 fallbackURL:config.eventDetailURL]
+                           customURL:cd.customURL ?: config.eventDetailURL
                          configState:configState
                             andReply:replyBlock];
       }

Source/santad/SNTNotificationQueue.h
@@ -37,7 +37,7 @@ using NotificationReplyBlock = void (^)(BOOL);
 
 - (void)addEvent:(SNTStoredExecutionEvent *)event
     withCustomMessage:(NSString *)message
-            customURL:(NSURL *)url
+            customURL:(NSString *)url
           configState:(SNTConfigState *)configState
              andReply:(void (^)(BOOL authenticated))reply;
 

Source/santad/SNTNotificationQueue.mm
@@ -49,7 +49,7 @@
 
 - (void)addEvent:(SNTStoredExecutionEvent *)event
     withCustomMessage:(NSString *)message
-            customURL:(NSURL *)url
+            customURL:(NSString *)url
           configState:(SNTConfigState *)configState
              andReply:(NotificationReplyBlock)replyBlock {
   if (!event) {

Source/common/SNTBlockMessageTest.mm
@@ -64,7 +64,7 @@
                       @"fbid=s.n.t&ti=SNT&si=SNT:s.n.t&ch=abc&"
                       @"un=my_un&mid=my_mid&hn=my_hn&u=my_u&s=my_s";
 
-  NSURL *gotUrl = [SNTBlockMessage eventDetailURLForEvent:se customURL:url fallbackURL:nil];
+  NSURL *gotUrl = [SNTBlockMessage eventDetailURLForEvent:se customURL:url];
 
   // Set fileBundleHash and test again for newly expected values
   se.fileBundleHash = @"my_fbh";
@@ -74,12 +74,12 @@
             @"fbid=s.n.t&ti=SNT&si=SNT:s.n.t&ch=abc&"
             @"un=my_un&mid=my_mid&hn=my_hn&u=my_u&s=my_s";
 
-  gotUrl = [SNTBlockMessage eventDetailURLForEvent:se customURL:url fallbackURL:nil];
+  gotUrl = [SNTBlockMessage eventDetailURLForEvent:se customURL:url];
 
   XCTAssertEqualObjects(gotUrl.absoluteString, wantUrl);
 
-  XCTAssertNil([SNTBlockMessage eventDetailURLForEvent:se customURL:nil fallbackURL:nil]);
-  XCTAssertNil([SNTBlockMessage eventDetailURLForEvent:se customURL:@"null" fallbackURL:nil]);
+  XCTAssertNil([SNTBlockMessage eventDetailURLForEvent:se customURL:nil]);
+  XCTAssertNil([SNTBlockMessage eventDetailURLForEvent:se customURL:@"null"]);
 }
 
 - (void)testEventDetailURLForFileAccessEvent {
@@ -120,7 +120,7 @@
   NSString *url = @"http://localhost?fi=%file_identifier%";
   NSString *wantUrl = @"http://localhost?fi=my_fi";
 
-  NSURL *gotUrl = [SNTBlockMessage eventDetailURLForEvent:se customURL:url fallbackURL:nil];
+  NSURL *gotUrl = [SNTBlockMessage eventDetailURLForEvent:se customURL:url];
 
   XCTAssertEqualObjects(gotUrl.absoluteString, wantUrl);
 }

Source/santad/SNTNotificationQueueTest.mm
@@ -56,7 +56,7 @@
   dispatch_semaphore_t sema = dispatch_semaphore_create(0);
   SNTStoredExecutionEvent *se = [[SNTStoredExecutionEvent alloc] init];
   NSString *customMessage = @"custom msg";
-  NSURL *customURL = [NSURL URLWithString:@"https://northpolesec.com"];
+  NSString *customURL = @"https://northpolesec.com";
 
   OCMExpect([self.mockProxy postBlockNotification:se
                                 withCustomMessage:customMessage
@@ -88,7 +88,7 @@
 - (void)testAddEventNil {
   dispatch_semaphore_t sema = dispatch_semaphore_create(0);
   NSString *customMessage = @"custom msg";
-  NSURL *customURL = [NSURL URLWithString:@"https://northpolesec.com"];
+  NSString *customURL = @"https://northpolesec.com";
 
   [self.sut addEvent:nil
       withCustomMessage:customMessage
@@ -113,7 +113,7 @@
 // well as posting messages for everything in the queue.
 - (void)testAddEventMulti {
   NSString *customMessage = @"custom msg";
-  NSURL *customURL = [NSURL URLWithString:@"https://northpolesec.com"];
+  NSString *customURL = @"https://northpolesec.com";
 
   SNTStoredExecutionEvent *se1 = [[SNTStoredExecutionEvent alloc] init];
   SNTStoredExecutionEvent *se2 = [[SNTStoredExecutionEvent alloc] init];
@@ -149,19 +149,19 @@
   NSMutableDictionary *d1 = [NSMutableDictionary dictionary];
   [d1 setValue:se1 forKey:@"event"];
   [d1 setValue:@"Message 1" forKey:@"message"];
-  [d1 setValue:[NSURL URLWithString:@"https://northpolesec.com/1"] forKey:@"url"];
+  [d1 setValue:@"https://northpolesec.com/1" forKey:@"url"];
   [d1 setValue:replyBlock1 forKey:@"reply"];
 
   NSMutableDictionary *d2 = [NSMutableDictionary dictionary];
   [d2 setValue:se2 forKey:@"event"];
   [d2 setValue:@"Message 2" forKey:@"message"];
-  [d2 setValue:[NSURL URLWithString:@"https://northpolesec.com/2"] forKey:@"url"];
+  [d2 setValue:@"https://northpolesec.com/2" forKey:@"url"];
   [d2 setValue:replyBlock2 forKey:@"reply"];
 
   NSMutableDictionary *d3 = [NSMutableDictionary dictionary];
   [d3 setValue:se3 forKey:@"event"];
   [d3 setValue:@"Message 3" forKey:@"message"];
-  [d3 setValue:[NSURL URLWithString:@"https://northpolesec.com/3"] forKey:@"url"];
+  [d3 setValue:@"https://northpolesec.com/3" forKey:@"url"];
   [d3 setValue:replyBlock3 forKey:@"reply"];
 
   self.ringbuf->Enqueue(d1);
@@ -172,19 +172,17 @@
   XCTAssertFalse(self.ringbuf->Empty());
 
   // postBlockNotification should never be called for `se1` since it will fall out of the ring
-  OCMVerify(never(), [self.mockProxy
-                         postBlockNotification:se1
-                             withCustomMessage:@"Message 1"
-                                     customURL:[NSURL URLWithString:@"https://northpolesec.com/1"]
-                                   configState:OCMOCK_ANY
-                                      andReply:OCMOCK_ANY]);
+  OCMVerify(never(), [self.mockProxy postBlockNotification:se1
+                                         withCustomMessage:@"Message 1"
+                                                 customURL:@"https://northpolesec.com/1"
+                                               configState:OCMOCK_ANY
+                                                  andReply:OCMOCK_ANY]);
 
-  OCMExpect([self.mockProxy
-                postBlockNotification:se2
-                    withCustomMessage:@"Message 2"
-                            customURL:[NSURL URLWithString:@"https://northpolesec.com/2"]
-                          configState:OCMOCK_ANY
-                             andReply:OCMOCK_ANY])
+  OCMExpect([self.mockProxy postBlockNotification:se2
+                                withCustomMessage:@"Message 2"
+                                        customURL:@"https://northpolesec.com/2"
+                                      configState:OCMOCK_ANY
+                                         andReply:OCMOCK_ANY])
       .andDo(^(NSInvocation *invocation) {
         void (^__unsafe_unretained replyBlock)(BOOL);
         [invocation getArgument:&replyBlock atIndex:6];
@@ -194,12 +192,11 @@
         });
       });
 
-  OCMExpect([self.mockProxy
-                postBlockNotification:se3
-                    withCustomMessage:@"Message 3"
-                            customURL:[NSURL URLWithString:@"https://northpolesec.com/3"]
-                          configState:OCMOCK_ANY
-                             andReply:OCMOCK_ANY])
+  OCMExpect([self.mockProxy postBlockNotification:se3
+                                withCustomMessage:@"Message 3"
+                                        customURL:@"https://northpolesec.com/3"
+                                      configState:OCMOCK_ANY
+                                         andReply:OCMOCK_ANY])
       .andDo(^(NSInvocation *invocation) {
         void (^__unsafe_unretained replyBlock)(BOOL);
         [invocation getArgument:&replyBlock atIndex:6];
@@ -261,20 +258,20 @@
   NSMutableDictionary *d1 = [NSMutableDictionary dictionary];
   [d1 setValue:se1 forKey:@"event"];
   [d1 setValue:@"Message 1" forKey:@"message"];
-  [d1 setValue:[NSURL URLWithString:@"https://northpolesec.com/1"] forKey:@"url"];
+  [d1 setValue:@"https://northpolesec.com/1" forKey:@"url"];
   [d1 setValue:[replyBlock1 copy] forKey:@"reply"];
 
   NSMutableDictionary *d2 = [NSMutableDictionary dictionary];
   [d2 setValue:se2 forKey:@"event"];
   [d2 setValue:@"Message 2" forKey:@"message"];
-  [d2 setValue:[NSURL URLWithString:@"https://northpolesec.com/2"] forKey:@"url"];
+  [d2 setValue:@"https://northpolesec.com/2" forKey:@"url"];
   [d2 setValue:[replyBlock2 copy] forKey:@"reply"];
 
   // Create dictionary with no reply block
   NSMutableDictionary *d3 = [NSMutableDictionary dictionary];
   [d3 setValue:se3 forKey:@"event"];
   [d3 setValue:@"Message 3" forKey:@"message"];
-  [d3 setValue:[NSURL URLWithString:@"https://northpolesec.com/3"] forKey:@"url"];
+  [d3 setValue:@"https://northpolesec.com/3" forKey:@"url"];
   // Intentionally not setting a reply block for d3
 
   self.ringbuf->Enqueue(d1);