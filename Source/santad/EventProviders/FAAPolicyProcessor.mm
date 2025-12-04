northpolesec/santa

commit ef4bb9f29e4c6ca233dc9817fcda7734ac7957fa

Switch target field in FileAccess messages to be FileInfo type (#680)
The purpose of this change is to switch the `target` field in
`FileAccess` messages from `FileInfoLight` to `FileInfo`. This is an
on-wire compatible change that is also both forward and backward
compatible with the telemetry proto schemas since the message types
overlap and the additional fields in `FileInfo` are optional.

The result will be that for supported `FileAccess` events -
specifically, those events that have a real target file - stat
information will now be included. This will not always be the case for
all file access events. For example, FAA events related to attempts to
create a file will not have stat information because the resultant file
has not yet been created.

Source/santad/EventProviders/FAAPolicyProcessor.mm
@@ -40,32 +40,6 @@ static constexpr size_t kPerProcessSetCapacity = 128;
 
 static constexpr uint32_t kOpenFlagsIndicatingWrite = FWRITE | O_APPEND | O_TRUNC;
 
-static inline std::string Path(const es_file_t *esFile) {
-  return std::string(esFile->path.data, esFile->path.length);
-}
-
-static inline std::string Path(const es_string_token_t &tok) {
-  return std::string(tok.data, tok.length);
-}
-
-static inline void PushBackIfNotTruncated(std::vector<FAAPolicyProcessor::PathTarget> &vec,
-                                          const es_file_t *esFile, bool isReadable = false) {
-  if (!esFile->path_truncated) {
-    vec.push_back({Path(esFile), isReadable,
-                   isReadable ? std::make_optional<std::pair<dev_t, ino_t>>(
-                                    {esFile->stat.st_dev, esFile->stat.st_ino})
-                              : std::nullopt});
-  }
-}
-
-// Note: This variant of PushBackIfNotTruncated can never be marked "is_readable"
-static inline void PushBackIfNotTruncated(std::vector<FAAPolicyProcessor::PathTarget> &vec,
-                                          const es_file_t *dir, const es_string_token_t &name) {
-  if (!dir->path_truncated) {
-    vec.push_back({Path(dir) + "/" + Path(name), false, std::nullopt});
-  }
-}
-
 bool IsBlockDecision(FileAccessPolicyDecision decision) {
   return decision == FileAccessPolicyDecision::kDenied ||
          decision == FileAccessPolicyDecision::kDeniedInvalidSignature;
@@ -295,7 +269,7 @@ SNTCachedDecision *FAAPolicyProcessor::GetCachedDecision(const struct stat &stat
 }
 
 bool FAAPolicyProcessor::PolicyAllowsReadsForTarget(
-    const Message &msg, const PathTarget &target,
+    const Message &msg, const Message::PathTarget &target,
     const std::shared_ptr<WatchItemPolicyBase> policy) {
   // All special cases currently require the option "allow_read_access" is set
   if (!policy->allow_read_access) {
@@ -335,7 +309,7 @@ bool FAAPolicyProcessor::PolicyAllowsReadsForTarget(
 }
 
 FileAccessPolicyDecision FAAPolicyProcessor::ApplyPolicy(
-    const Message &msg, const PathTarget &target,
+    const Message &msg, const Message::PathTarget &target,
     const std::optional<std::shared_ptr<WatchItemPolicyBase>> optional_policy,
     CheckIfPolicyMatchesBlock check_if_policy_matches_block) {
   if (!optional_policy.has_value()) {
@@ -388,8 +362,8 @@ FileAccessPolicyDecision FAAPolicyProcessor::ApplyPolicy(
   return decision;
 }
 
-void FAAPolicyProcessor::LogTelemetry(const WatchItemPolicyBase &policy, const PathTarget &target,
-                                      const Message &msg, FileAccessPolicyDecision decision) {
+void FAAPolicyProcessor::LogTelemetry(const WatchItemPolicyBase &policy, const Message &msg,
+                                      size_t target_index, FileAccessPolicyDecision decision) {
   RateLimiter::Decision rate_limit_decision = rate_limiter_.Decide(msg->mach_time);
   metrics_->SetFileAccessEventMetrics(policy.version, policy.name,
                                       (rate_limit_decision == RateLimiter::Decision::kAllowed)
@@ -405,14 +379,17 @@ void FAAPolicyProcessor::LogTelemetry(const WatchItemPolicyBase &policy, const P
   // they have proper lifetimes.
   std::string policy_name_copy = policy.name;
   std::string policy_version_copy = policy.version;
-  std::string target_path_copy = target.path;
   __block Message msg_copy(msg);
 
   dispatch_async(queue_, ^{
+    Message moved_in_msg = std::move(msg_copy);
+    const Message::PathTarget &target = msg.PathTargetAtIndex(target_index);
     EnrichedProcess enriched_proc =
-        enricher_->Enrich(*msg_copy->process, EnrichOptions::kLocalOnly);
-    logger_->LogFileAccess(policy_version_copy, policy_name_copy, std::move(msg_copy),
-                           std::move(enriched_proc), target_path_copy, decision);
+        enricher_->Enrich(*moved_in_msg->process, EnrichOptions::kLocalOnly);
+    std::optional<santa::EnrichedFile> enriched_event_target =
+        enricher_->Enrich(target.unsafe_file, EnrichOptions::kLocalOnly);
+    logger_->LogFileAccess(policy_version_copy, policy_name_copy, std::move(moved_in_msg),
+                           enriched_proc, target_index, std::move(enriched_event_target), decision);
   });
 }
 
@@ -456,11 +433,13 @@ void FAAPolicyProcessor::LogTTY(SNTStoredFileAccessEvent *event, URLTextPair lin
 }
 
 FileAccessPolicyDecision FAAPolicyProcessor::ProcessTargetAndPolicy(
-    const Message &msg, const PathTarget &target,
-    const std::optional<std::shared_ptr<WatchItemPolicyBase>> optional_policy,
+    const Message &msg, const TargetPolicyPair &target_policy_pair,
     CheckIfPolicyMatchesBlock check_if_policy_matches_block,
     SNTFileAccessDeniedBlock file_access_denied_block,
     SNTOverrideFileAccessAction override_action) {
+  const Message::PathTarget &target = msg.PathTargetAtIndex(target_policy_pair.first);
+  const std::optional<std::shared_ptr<WatchItemPolicyBase>> optional_policy =
+      target_policy_pair.second;
   FileAccessPolicyDecision decision = ApplyOverrideToDecision(
       ApplyPolicy(msg, target, optional_policy, check_if_policy_matches_block), override_action);
 
@@ -469,7 +448,7 @@ FileAccessPolicyDecision FAAPolicyProcessor::ProcessTargetAndPolicy(
   if (ShouldLogDecision(decision) && optional_policy.has_value()) {
     std::shared_ptr<WatchItemPolicyBase> policy = *optional_policy;
 
-    LogTelemetry(*policy, target, msg, decision);
+    LogTelemetry(*policy, msg, target_policy_pair.first, decision);
 
     SNTCachedDecision *cd = GetCachedDecision(msg->process->executable->stat);
     SNTStoredFileAccessEvent *event = [[SNTStoredFileAccessEvent alloc] init];
@@ -529,9 +508,10 @@ FAAPolicyProcessor::ESResult FAAPolicyProcessor::ProcessMessage(
   bool cacheable = true;
 
   for (const TargetPolicyPair &target_policy_pair : target_policy_pairs) {
-    FileAccessPolicyDecision decision = ProcessTargetAndPolicy(
-        msg, target_policy_pair.first, target_policy_pair.second, check_if_policy_matches_block,
-        file_access_denied_block, overrideAction);
+    const Message::PathTarget &path_target = msg.PathTargetAtIndex(target_policy_pair.first);
+    FileAccessPolicyDecision decision =
+        ProcessTargetAndPolicy(msg, target_policy_pair, check_if_policy_matches_block,
+                               file_access_denied_block, overrideAction);
     // Populate the reads_cache_ if:
     //   1. The policy applied
     //   2. The process wasn't invalid
@@ -540,11 +520,12 @@ FAAPolicyProcessor::ESResult FAAPolicyProcessor::ProcessMessage(
     // Note: As long as a policy allows read access, the caller's read cache can be updated
     // regardless of the RuleType of the policy.
     if (decision != FileAccessPolicyDecision::kNoPolicy &&
-        decision != FileAccessPolicyDecision::kDeniedInvalidSignature &&
-        target_policy_pair.first.devno_ino.has_value() && target_policy_pair.second.has_value() &&
+        decision != FileAccessPolicyDecision::kDeniedInvalidSignature && path_target.is_readable &&
+        path_target.unsafe_file && target_policy_pair.second.has_value() &&
         (*target_policy_pair.second)->allow_read_access) {
       reads_cache_.Set(MakeReadsCacheKey(msg->process->audit_token, client_type),
-                       *target_policy_pair.first.devno_ino);
+                       std::pair<dev_t, ino_t>({path_target.unsafe_file->stat.st_dev,
+                                                path_target.unsafe_file->stat.st_ino}));
     }
 
     policy_result =
@@ -581,82 +562,6 @@ std::optional<FAAPolicyProcessor::ESResult> FAAPolicyProcessor::ImmediateRespons
 void FAAPolicyProcessor::NotifyExit(const audit_token_t &tok, FAAClientType client_type) {
   reads_cache_.Remove(MakeReadsCacheKey(tok, client_type));
   tty_message_cache_.Remove(PidPidversion(tok));
-}
-
-std::vector<FAAPolicyProcessor::PathTarget> FAAPolicyProcessor::PathTargets(const Message &msg) {
-  std::vector<FAAPolicyProcessor::PathTarget> targets;
-  targets.reserve(2);
-
-  switch (msg->event_type) {
-    case ES_EVENT_TYPE_AUTH_CLONE:
-      PushBackIfNotTruncated(targets, msg->event.clone.source, true);
-      PushBackIfNotTruncated(targets, msg->event.clone.target_dir, msg->event.clone.target_name);
-      break;
-
-    case ES_EVENT_TYPE_AUTH_CREATE:
-      // AUTH CREATE events should always be ES_DESTINATION_TYPE_NEW_PATH
-      if (msg->event.create.destination_type == ES_DESTINATION_TYPE_NEW_PATH) {
-        PushBackIfNotTruncated(targets, msg->event.create.destination.new_path.dir,
-                               msg->event.create.destination.new_path.filename);
-      } else {
-        LOGW(@"Unexpected destination type for create event: %d. Ignoring target.",
-             msg->event.create.destination_type);
-      }
-      break;
-
-    case ES_EVENT_TYPE_AUTH_COPYFILE:
-      PushBackIfNotTruncated(targets, msg->event.copyfile.source, true);
-      if (msg->event.copyfile.target_file) {
-        PushBackIfNotTruncated(targets, msg->event.copyfile.target_file);
-      } else {
-        PushBackIfNotTruncated(targets, msg->event.copyfile.target_dir,
-                               msg->event.copyfile.target_name);
-      }
-      break;
-
-    case ES_EVENT_TYPE_AUTH_EXCHANGEDATA:
-      PushBackIfNotTruncated(targets, msg->event.exchangedata.file1);
-      PushBackIfNotTruncated(targets, msg->event.exchangedata.file2);
-      break;
-
-    case ES_EVENT_TYPE_AUTH_LINK:
-      PushBackIfNotTruncated(targets, msg->event.link.source);
-      PushBackIfNotTruncated(targets, msg->event.link.target_dir, msg->event.link.target_filename);
-      break;
-
-    case ES_EVENT_TYPE_AUTH_OPEN:
-      PushBackIfNotTruncated(targets, msg->event.open.file, true);
-      break;
-
-    case ES_EVENT_TYPE_AUTH_RENAME:
-      PushBackIfNotTruncated(targets, msg->event.rename.source);
-      if (msg->event.rename.destination_type == ES_DESTINATION_TYPE_EXISTING_FILE) {
-        PushBackIfNotTruncated(targets, msg->event.rename.destination.existing_file);
-      } else if (msg->event.rename.destination_type == ES_DESTINATION_TYPE_NEW_PATH) {
-        PushBackIfNotTruncated(targets, msg->event.rename.destination.new_path.dir,
-                               msg->event.rename.destination.new_path.filename);
-      } else {
-        LOGW(@"Unexpected destination type for rename event: %d. Ignoring destination.",
-             msg->event.rename.destination_type);
-      }
-      break;
-
-    case ES_EVENT_TYPE_AUTH_TRUNCATE:
-      PushBackIfNotTruncated(targets, msg->event.truncate.target);
-      break;
-
-    case ES_EVENT_TYPE_AUTH_UNLINK:
-      PushBackIfNotTruncated(targets, msg->event.unlink.target);
-      break;
-
-    default:
-      [NSException
-           raise:@"Unexpected event type"
-          format:@"File Access Authorizer client does not handle event: %d", msg->event_type];
-      exit(EXIT_FAILURE);
-  }
-
-  return targets;
 }
 
 }  // namespace santa

Source/common/santa.proto
@@ -532,7 +532,7 @@ message FileAccess {
   optional ProcessInfo instigator = 1;
 
   // The path that was accessed
-  optional FileInfoLight target = 2;
+  optional FileInfo target = 2;
 
   // The version of the policy when the decision was made
   optional string policy_version = 3;

Source/santad/EventProviders/EndpointSecurity/Enricher.h
@@ -46,6 +46,9 @@ class Enricher {
       EnrichOptions options = EnrichOptions::kDefault);
   virtual EnrichedFile Enrich(const es_file_t &es_file,
                               EnrichOptions options = EnrichOptions::kDefault);
+  virtual std::optional<EnrichedFile> Enrich(
+      const es_file_t *es_file,
+      EnrichOptions options = EnrichOptions::kDefault);
 
   virtual std::optional<std::shared_ptr<std::string>> UsernameForUID(
       uid_t uid, EnrichOptions options = EnrichOptions::kDefault);

Source/santad/EventProviders/EndpointSecurity/Enricher.mm
@@ -211,6 +211,10 @@ EnrichedProcess Enricher::Enrich(const es_process_t &es_proc, EnrichOptions opti
                     : std::nullopt);
 }
 
+std::optional<EnrichedFile> Enricher::Enrich(const es_file_t *es_file, EnrichOptions options) {
+  return es_file ? std::make_optional<EnrichedFile>(Enrich(*es_file, options)) : std::nullopt;
+}
+
 EnrichedFile Enricher::Enrich(const es_file_t &es_file, EnrichOptions options) {
   // TODO(mlw): Consider having the enricher perform file hashing. This will
   // make more sense if we start including hashes in more event types.

Source/santad/EventProviders/EndpointSecurity/Message.h
@@ -29,6 +29,17 @@ class EndpointSecurityAPI;
 
 class Message {
  public:
+  // Small structure to hold event target information.
+  struct PathTarget {
+    std::string path;
+    bool is_readable;
+    // This is a pointer into an es_message_t. The message must be valid for
+    // this pointer to be valid. The interfaces in the Message class will vend
+    // pointers that are valid, and callers must not store or otherwise
+    // reference the pointer to ensure no valid access is made.
+    const es_file_t* unsafe_file;
+  };
+
   Message(std::shared_ptr<EndpointSecurityAPI> esapi,
           const es_message_t* es_msg);
   ~Message();
@@ -55,13 +66,26 @@ class Message {
   std::string ParentProcessName() const;
   std::string ParentProcessPath() const;
 
+  // This method is not thread safe until after the first completed call.
+  const std::vector<Message::PathTarget> PathTargets();
+
+  inline bool HasPathTarget(size_t index) const {
+    return index < path_targets_.size();
+  }
+
+  inline const Message::PathTarget& PathTargetAtIndex(size_t index) const {
+    return path_targets_.at(index);
+  }
+
  private:
+  std::string GetProcessName(pid_t pid) const;
+  std::string GetProcessPath(audit_token_t* tok) const;
+  void PopulatePathTargets();
+
   std::shared_ptr<EndpointSecurityAPI> esapi_;
   const es_message_t* es_msg_;
   std::optional<santa::santad::process_tree::ProcessToken> process_token_;
-
-  std::string GetProcessName(pid_t pid) const;
-  std::string GetProcessPath(audit_token_t* tok) const;
+  std::vector<PathTarget> path_targets_;
 };
 
 }  // namespace santa

Source/santad/EventProviders/EndpointSecurity/Message.mm
@@ -24,6 +24,29 @@
 
 namespace santa {
 
+static inline std::string Path(const es_file_t *esFile) {
+  return std::string(esFile->path.data, esFile->path.length);
+}
+
+static inline std::string Path(const es_string_token_t &tok) {
+  return std::string(tok.data, tok.length);
+}
+
+static inline void PushBackIfNotTruncated(std::vector<Message::PathTarget> &vec,
+                                          const es_file_t *esFile, bool isReadable = false) {
+  if (!esFile->path_truncated) {
+    vec.push_back({Path(esFile), isReadable, esFile});
+  }
+}
+
+// Note: This variant of PushBackIfNotTruncated can never be marked "is_readable"
+static inline void PushBackIfNotTruncated(std::vector<Message::PathTarget> &vec,
+                                          const es_file_t *dir, const es_string_token_t &name) {
+  if (!dir->path_truncated) {
+    vec.push_back({Path(dir) + "/" + Path(name), false, nullptr});
+  }
+}
+
 Message::Message(std::shared_ptr<EndpointSecurityAPI> esapi, const es_message_t *es_msg)
     : esapi_(std::move(esapi)), es_msg_(es_msg), process_token_(std::nullopt) {
   esapi_->RetainMessage(es_msg);
@@ -38,6 +61,7 @@ Message::~Message() {
 Message::Message(Message &&other) {
   esapi_ = std::move(other.esapi_);
   es_msg_ = other.es_msg_;
+  path_targets_ = std::move(other.path_targets_);
   other.es_msg_ = nullptr;
   process_token_ = std::move(other.process_token_);
   other.process_token_ = std::nullopt;
@@ -47,6 +71,7 @@ Message::Message(const Message &other) {
   esapi_ = other.esapi_;
   es_msg_ = other.es_msg_;
   esapi_->RetainMessage(es_msg_);
+  path_targets_ = other.path_targets_;
   process_token_ = other.process_token_;
 }
 
@@ -80,6 +105,88 @@ std::string Message::GetProcessPath(audit_token_t *tok) const {
   } else {
     return std::string("");
   }
+}
+
+const std::vector<Message::PathTarget> Message::PathTargets() {
+  if (path_targets_.size() == 0) {
+    PopulatePathTargets();
+  }
+
+  return path_targets_;
+}
+
+void Message::PopulatePathTargets() {
+  std::vector<Message::PathTarget> targets;
+  targets.reserve(2);
+
+  switch (es_msg_->event_type) {
+    case ES_EVENT_TYPE_AUTH_CLONE:
+      PushBackIfNotTruncated(targets, es_msg_->event.clone.source, true);
+      PushBackIfNotTruncated(targets, es_msg_->event.clone.target_dir,
+                             es_msg_->event.clone.target_name);
+      break;
+
+    case ES_EVENT_TYPE_AUTH_CREATE:
+      // AUTH CREATE events should always be ES_DESTINATION_TYPE_NEW_PATH
+      if (es_msg_->event.create.destination_type == ES_DESTINATION_TYPE_NEW_PATH) {
+        PushBackIfNotTruncated(targets, es_msg_->event.create.destination.new_path.dir,
+                               es_msg_->event.create.destination.new_path.filename);
+      } else {
+        LOGW(@"Unexpected destination type for create event: %d. Ignoring target.",
+             es_msg_->event.create.destination_type);
+      }
+      break;
+
+    case ES_EVENT_TYPE_AUTH_COPYFILE:
+      PushBackIfNotTruncated(targets, es_msg_->event.copyfile.source, true);
+      if (es_msg_->event.copyfile.target_file) {
+        PushBackIfNotTruncated(targets, es_msg_->event.copyfile.target_file);
+      } else {
+        PushBackIfNotTruncated(targets, es_msg_->event.copyfile.target_dir,
+                               es_msg_->event.copyfile.target_name);
+      }
+      break;
+
+    case ES_EVENT_TYPE_AUTH_EXCHANGEDATA:
+      PushBackIfNotTruncated(targets, es_msg_->event.exchangedata.file1);
+      PushBackIfNotTruncated(targets, es_msg_->event.exchangedata.file2);
+      break;
+
+    case ES_EVENT_TYPE_AUTH_LINK:
+      PushBackIfNotTruncated(targets, es_msg_->event.link.source);
+      PushBackIfNotTruncated(targets, es_msg_->event.link.target_dir,
+                             es_msg_->event.link.target_filename);
+      break;
+
+    case ES_EVENT_TYPE_AUTH_OPEN:
+      PushBackIfNotTruncated(targets, es_msg_->event.open.file, true);
+      break;
+
+    case ES_EVENT_TYPE_AUTH_RENAME:
+      PushBackIfNotTruncated(targets, es_msg_->event.rename.source);
+      if (es_msg_->event.rename.destination_type == ES_DESTINATION_TYPE_EXISTING_FILE) {
+        PushBackIfNotTruncated(targets, es_msg_->event.rename.destination.existing_file);
+      } else if (es_msg_->event.rename.destination_type == ES_DESTINATION_TYPE_NEW_PATH) {
+        PushBackIfNotTruncated(targets, es_msg_->event.rename.destination.new_path.dir,
+                               es_msg_->event.rename.destination.new_path.filename);
+      } else {
+        LOGW(@"Unexpected destination type for rename event: %d. Ignoring destination.",
+             es_msg_->event.rename.destination_type);
+      }
+      break;
+
+    case ES_EVENT_TYPE_AUTH_TRUNCATE:
+      PushBackIfNotTruncated(targets, es_msg_->event.truncate.target);
+      break;
+
+    case ES_EVENT_TYPE_AUTH_UNLINK:
+      PushBackIfNotTruncated(targets, es_msg_->event.unlink.target);
+      break;
+
+    default: break;
+  }
+
+  path_targets_.swap(targets);
 }
 
 }  // namespace santa

Source/santad/EventProviders/EndpointSecurity/MessageTest.mm
@@ -184,4 +184,217 @@ std::string GetProcessPath(pid_t pid) {
   }
 }
 
+- (void)testPathTargets {
+  // This test ensures that the `GetPathTargets` functions returns the
+  // expected combination of targets for each handled event variant
+  es_file_t testFile1 = MakeESFile("test_file_1", MakeStat(100));
+  es_file_t testFile2 = MakeESFile("test_file_2", MakeStat(200));
+  es_file_t testDir = MakeESFile("test_dir", MakeStat(300));
+  es_string_token_t testTok = MakeESStringToken("test_tok");
+  std::string dirTok = std::string(testDir.path.data) + "/" + std::string(testTok.data);
+
+  es_message_t esMsg;
+
+  auto mockESApi = std::make_shared<MockEndpointSecurityAPI>();
+  mockESApi->SetExpectationsRetainReleaseMessage();
+
+  {
+    Message msg(mockESApi, &esMsg);
+    esMsg.event_type = ES_EVENT_TYPE_AUTH_OPEN;
+    esMsg.event.open.file = &testFile1;
+
+    std::vector<Message::PathTarget> targets = msg.PathTargets();
+
+    XCTAssertEqual(targets.size(), 1);
+    XCTAssertCStringEqual(targets[0].path.c_str(), testFile1.path.data);
+    XCTAssertTrue(targets[0].is_readable);
+    XCTAssertEqual(targets[0].unsafe_file, &testFile1);
+    XCTAssertTrue(msg.HasPathTarget(0));
+    XCTAssertFalse(msg.HasPathTarget(1));
+  }
+
+  {
+    Message msg(mockESApi, &esMsg);
+    esMsg.event_type = ES_EVENT_TYPE_AUTH_LINK;
+    esMsg.event.link.source = &testFile1;
+    esMsg.event.link.target_dir = &testDir;
+    esMsg.event.link.target_filename = testTok;
+
+    std::vector<Message::PathTarget> targets = msg.PathTargets();
+
+    XCTAssertEqual(targets.size(), 2);
+    XCTAssertCStringEqual(targets[0].path.c_str(), testFile1.path.data);
+    XCTAssertFalse(targets[0].is_readable);
+    XCTAssertEqual(targets[0].unsafe_file, &testFile1);
+    XCTAssertCppStringEqual(targets[1].path, dirTok);
+    XCTAssertFalse(targets[1].is_readable);
+    XCTAssertEqual(targets[1].unsafe_file, nullptr);
+    XCTAssertTrue(msg.HasPathTarget(0));
+    XCTAssertTrue(msg.HasPathTarget(1));
+    XCTAssertFalse(msg.HasPathTarget(2));
+  }
+
+  {
+    esMsg.event_type = ES_EVENT_TYPE_AUTH_RENAME;
+    esMsg.event.rename.source = &testFile1;
+
+    {
+      Message msg(mockESApi, &esMsg);
+      esMsg.event.rename.destination_type = ES_DESTINATION_TYPE_EXISTING_FILE;
+      esMsg.event.rename.destination.existing_file = &testFile2;
+
+      std::vector<Message::PathTarget> targets = msg.PathTargets();
+
+      XCTAssertEqual(targets.size(), 2);
+      XCTAssertCStringEqual(targets[0].path.c_str(), testFile1.path.data);
+      XCTAssertFalse(targets[0].is_readable);
+      XCTAssertEqual(targets[0].unsafe_file, &testFile1);
+      XCTAssertCStringEqual(targets[1].path.c_str(), testFile2.path.data);
+      XCTAssertFalse(targets[1].is_readable);
+      XCTAssertEqual(targets[1].unsafe_file, &testFile2);
+    }
+
+    {
+      Message msg(mockESApi, &esMsg);
+      esMsg.event.rename.destination_type = ES_DESTINATION_TYPE_NEW_PATH;
+      esMsg.event.rename.destination.new_path.dir = &testDir;
+      esMsg.event.rename.destination.new_path.filename = testTok;
+
+      std::vector<Message::PathTarget> targets = msg.PathTargets();
+
+      XCTAssertEqual(targets.size(), 2);
+      XCTAssertCStringEqual(targets[0].path.c_str(), testFile1.path.data);
+      XCTAssertFalse(targets[0].is_readable);
+      XCTAssertEqual(targets[0].unsafe_file, &testFile1);
+      XCTAssertCppStringEqual(targets[1].path, dirTok);
+      XCTAssertFalse(targets[1].is_readable);
+      XCTAssertEqual(targets[1].unsafe_file, nullptr);
+    }
+  }
+
+  {
+    Message msg(mockESApi, &esMsg);
+    esMsg.event_type = ES_EVENT_TYPE_AUTH_UNLINK;
+    esMsg.event.unlink.target = &testFile1;
+
+    std::vector<Message::PathTarget> targets = msg.PathTargets();
+
+    XCTAssertEqual(targets.size(), 1);
+    XCTAssertCStringEqual(targets[0].path.c_str(), testFile1.path.data);
+    XCTAssertFalse(targets[0].is_readable);
+    XCTAssertEqual(targets[0].unsafe_file, &testFile1);
+  }
+
+  {
+    Message msg(mockESApi, &esMsg);
+    esMsg.event_type = ES_EVENT_TYPE_AUTH_CLONE;
+    esMsg.event.clone.source = &testFile1;
+    esMsg.event.clone.target_dir = &testDir;
+    esMsg.event.clone.target_name = testTok;
+
+    std::vector<Message::PathTarget> targets = msg.PathTargets();
+
+    XCTAssertEqual(targets.size(), 2);
+    XCTAssertCStringEqual(targets[0].path.c_str(), testFile1.path.data);
+    XCTAssertTrue(targets[0].is_readable);
+    XCTAssertEqual(targets[0].unsafe_file, &testFile1);
+    XCTAssertCppStringEqual(targets[1].path, dirTok);
+    XCTAssertFalse(targets[1].is_readable);
+    XCTAssertEqual(targets[1].unsafe_file, nullptr);
+  }
+
+  {
+    Message msg(mockESApi, &esMsg);
+    esMsg.event_type = ES_EVENT_TYPE_AUTH_EXCHANGEDATA;
+    esMsg.event.exchangedata.file1 = &testFile1;
+    esMsg.event.exchangedata.file2 = &testFile2;
+
+    std::vector<Message::PathTarget> targets = msg.PathTargets();
+
+    XCTAssertEqual(targets.size(), 2);
+    XCTAssertCStringEqual(targets[0].path.c_str(), testFile1.path.data);
+    XCTAssertFalse(targets[0].is_readable);
+    XCTAssertEqual(targets[0].unsafe_file, &testFile1);
+    XCTAssertCStringEqual(targets[1].path.c_str(), testFile2.path.data);
+    XCTAssertFalse(targets[1].is_readable);
+    XCTAssertEqual(targets[1].unsafe_file, &testFile2);
+  }
+
+  {
+    Message msg(mockESApi, &esMsg);
+    esMsg.event_type = ES_EVENT_TYPE_AUTH_CREATE;
+    esMsg.event.create.destination_type = ES_DESTINATION_TYPE_NEW_PATH;
+    esMsg.event.create.destination.new_path.dir = &testDir;
+    esMsg.event.create.destination.new_path.filename = testTok;
+
+    std::vector<Message::PathTarget> targets = msg.PathTargets();
+
+    XCTAssertEqual(targets.size(), 1);
+    XCTAssertCppStringEqual(targets[0].path, dirTok);
+    XCTAssertFalse(targets[0].is_readable);
+    XCTAssertEqual(targets[0].unsafe_file, nullptr);
+  }
+
+  {
+    Message msg(mockESApi, &esMsg);
+    esMsg.event_type = ES_EVENT_TYPE_AUTH_TRUNCATE;
+    esMsg.event.truncate.target = &testFile1;
+
+    std::vector<Message::PathTarget> targets = msg.PathTargets();
+
+    XCTAssertEqual(targets.size(), 1);
+    XCTAssertCStringEqual(targets[0].path.c_str(), testFile1.path.data);
+    XCTAssertFalse(targets[0].is_readable);
+    XCTAssertEqual(targets[0].unsafe_file, &testFile1);
+  }
+
+  {
+    esMsg.event_type = ES_EVENT_TYPE_AUTH_COPYFILE;
+    esMsg.event.copyfile.source = &testFile1;
+    esMsg.event.copyfile.target_dir = &testDir;
+    esMsg.event.copyfile.target_name = testTok;
+
+    {
+      Message msg(mockESApi, &esMsg);
+      esMsg.event.copyfile.target_file = nullptr;
+
+      std::vector<Message::PathTarget> targets = msg.PathTargets();
+
+      XCTAssertEqual(targets.size(), 2);
+      XCTAssertCStringEqual(targets[0].path.c_str(), testFile1.path.data);
+      XCTAssertTrue(targets[0].is_readable);
+      XCTAssertEqual(targets[0].unsafe_file, &testFile1);
+      XCTAssertCppStringEqual(targets[1].path, dirTok);
+      XCTAssertFalse(targets[1].is_readable);
+      XCTAssertEqual(targets[1].unsafe_file, nullptr);
+    }
+
+    {
+      Message msg(mockESApi, &esMsg);
+      esMsg.event.copyfile.target_file = &testFile2;
+
+      std::vector<Message::PathTarget> targets = msg.PathTargets();
+
+      XCTAssertEqual(targets.size(), 2);
+      XCTAssertCStringEqual(targets[0].path.c_str(), testFile1.path.data);
+      XCTAssertTrue(targets[0].is_readable);
+      XCTAssertEqual(targets[0].unsafe_file, &testFile1);
+      XCTAssertCStringEqual(targets[1].path.c_str(), testFile2.path.data);
+      XCTAssertFalse(targets[1].is_readable);
+      XCTAssertEqual(targets[1].unsafe_file, &testFile2);
+    }
+  }
+
+  // Test some event type without any targets
+  {
+    Message msg(mockESApi, &esMsg);
+    esMsg.event_type = ES_EVENT_TYPE_NOTIFY_OD_DELETE_GROUP;
+
+    std::vector<Message::PathTarget> targets = msg.PathTargets();
+
+    XCTAssertEqual(targets.size(), 0);
+    XCTAssertFalse(msg.HasPathTarget(0));
+  }
+}
+
 @end

commit 8e2757336161949eb9bbd6cf114befa224f84067

Fix issue in FAA logging that could cause a crash (unreleased) (#684)
Bug introduced by #680, was not yet part of a release.

Part of SNT-217

Source/santad/EventProviders/FAAPolicyProcessor.mm
@@ -383,7 +383,7 @@ void FAAPolicyProcessor::LogTelemetry(const WatchItemPolicyBase &policy, const M
 
   dispatch_async(queue_, ^{
     Message moved_in_msg = std::move(msg_copy);
-    const Message::PathTarget &target = msg.PathTargetAtIndex(target_index);
+    const Message::PathTarget &target = moved_in_msg.PathTargetAtIndex(target_index);
     EnrichedProcess enriched_proc =
         enricher_->Enrich(*moved_in_msg->process, EnrichOptions::kLocalOnly);
     std::optional<santa::EnrichedFile> enriched_event_target =