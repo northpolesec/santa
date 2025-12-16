northpolesec/santa

commit 7e183cb688066e0f6471f557c5b033d7792e7e9b

Fix rule download issue where success/failure was improperly determined (#695)
Success/failure is defined by the result of the bool method, not the
presence of NSError, which is also used for warnings. This change also
allows bubbling up multiple errors that can be reported.

---------

Signed-off-by: Matt W <436037+mlw@users.noreply.github.com>
Co-authored-by: Russell Hancox <russellhancox@users.noreply.github.com>

Source/santactl/Commands/SNTCommandRule.mm
@@ -290,9 +290,15 @@ REGISTER_COMMAND_NAME(@"rule")
                       fileAccessRules:nil
                           ruleCleanup:cleanupType
                                source:SNTRuleAddSourceSantactl
-                                reply:^(NSError *error) {
-                                  TEE_LOGE(@"Failed to delete rules: %@\n",
-                                           error.localizedDescription);
+                                reply:^(BOOL success, NSArray<NSError *> *errors) {
+                                  if (success) {
+                                    TEE_LOGE(@"Rules were deleted, with the following warnings:");
+                                  } else {
+                                    TEE_LOGE(@"Failed to delete rules:");
+                                  }
+                                  for (NSError *e in errors) {
+                                    TEE_LOGE(@"\t%@", e.localizedDescription);
+                                  }
                                   exit(EXIT_FAILURE);
                                 }];
     exit(EXIT_SUCCESS);
@@ -394,12 +400,20 @@ REGISTER_COMMAND_NAME(@"rule")
                     fileAccessRules:nil
                         ruleCleanup:SNTRuleCleanupNone
                              source:SNTRuleAddSourceSantactl
-                              reply:^(NSError *error) {
-                                if (error) {
-                                  TEE_LOGE(@"Failed to modify rules: %@",
-                                           error.localizedFailureReason);
+                              reply:^(BOOL success, NSArray<NSError *> *errors) {
+                                if (!errors) {
+                                  TEE_LOGE(@"Failed to modify rules:");
+                                  for (NSError *e in errors) {
+                                    TEE_LOGE(@"\t%@", e.localizedFailureReason);
+                                  }
                                   exit(1);
                                 } else {
+                                  if (errors.count > 0) {
+                                    TEE_LOGE(@"Rules were modified but with the following issues:");
+                                    for (NSError *e in errors) {
+                                      TEE_LOGE(@"\t%@", e.localizedFailureReason);
+                                    }
+                                  }
                                   NSString *ruleType;
                                   switch (newRule.type) {
                                     case SNTRuleTypeCertificate:
@@ -498,13 +512,17 @@ REGISTER_COMMAND_NAME(@"rule")
                     fileAccessRules:nil
                         ruleCleanup:cleanupType
                              source:SNTRuleAddSourceSantactl
-                              reply:^(NSError *error) {
-                                if (error) {
-                                  TEE_LOGE(@"Failed to modify rules: %@",
-                                           error.localizedFailureReason);
-                                  exit(1);
+                              reply:^(BOOL success, NSArray<NSError *> *errors) {
+                                if (success) {
+                                  TEE_LOGE(@"Rules were modified but with the following issues:");
+                                } else {
+                                  TEE_LOGE(@"Failed to modify rules:");
                                 }
-                                exit(0);
+                                for (NSError *e in errors) {
+                                  TEE_LOGE(@"\t%@", e.localizedFailureReason);
+                                }
+
+                                exit(success == NO ? EXIT_FAILURE : EXIT_SUCCESS);
                               }];
 }
 

Source/common/SNTError.h
@@ -60,11 +60,22 @@ typedef NS_ENUM(NSInteger, SNTErrorCode) {
               message:(nonnull NSString *)msg
                detail:(nonnull NSString *)detail;
 
+// Return a new error using the provided code, message, and details. `msg` will populate the
+// NSLocalizedErrorDescription key. `detail` will populate the NSLocalizedFailureReasonErrorKey key.
++ (nullable NSError *)createErrorWithCode:(SNTErrorCode)code
+                                  message:(nonnull NSString *)msg
+                                   detail:(nonnull NSString *)detail;
+
 // Generate a new error with the provided code and format-string message, and populate into the
 // provided NSError; `msg` will populate the NSLocalizedErrorDescription key.
 + (void)populateError:(NSError *_Nullable *_Nullable)error
              withCode:(SNTErrorCode)code
                format:(nonnull NSString *)format, ... NS_FORMAT_FUNCTION(3, 4);
+
+// Return a new NSError with the provided format string message and error code.
+// `msg` will populate the NSLocalizedErrorDescription key.
++ (nullable NSError *)createErrorWithCode:(SNTErrorCode)code
+                                   format:(nonnull NSString *)format, ... NS_FORMAT_FUNCTION(2, 3);
 
 // Generate a new error with the provided format-string message, and populate into the provided
 // NSError; `msg` will populate the NSLocalizedErrorDescription key. The error code will be

Source/common/SNTError.mm
@@ -33,6 +33,14 @@ const NSErrorDomain SantaErrorDomain = @"com.northpolesec.santa.error";
                            }];
 }
 
++ (nullable NSError *)createErrorWithCode:(SNTErrorCode)code
+                                  message:(nonnull NSString *)msg
+                                   detail:(nonnull NSString *)detail {
+  NSError *err;
+  [self populateError:&err withCode:code message:msg detail:detail];
+  return err;
+}
+
 + (void)populateError:(NSError **)error
              withCode:(SNTErrorCode)code
                format:(nonnull NSString *)format, ... NS_FORMAT_FUNCTION(3, 4) {
@@ -46,6 +54,20 @@ const NSErrorDomain SantaErrorDomain = @"com.northpolesec.santa.error";
   *error = [NSError errorWithDomain:SantaErrorDomain
                                code:code
                            userInfo:@{NSLocalizedDescriptionKey : msg}];
+}
+
++ (nullable NSError *)createErrorWithCode:(SNTErrorCode)code
+                                   format:(nonnull NSString *)format, ... NS_FORMAT_FUNCTION(2, 3) {
+  NSError *error;
+
+  va_list args;
+  va_start(args, format);
+  NSString *msg = [[NSString alloc] initWithFormat:format arguments:args];
+  va_end(args);
+
+  [self populateError:&error withCode:code format:@"%@", msg];
+
+  return error;
 }
 
 + (void)populateError:(NSError **)error

Source/common/SNTXPCControlInterface.h
@@ -42,7 +42,7 @@ typedef NS_ENUM(NSInteger, SNTRuleAddSource) {
                       fileAccessRules:(NSArray<SNTFileAccessRule *> *)fileAccessRules
                           ruleCleanup:(SNTRuleCleanup)cleanupType
                                source:(SNTRuleAddSource)source
-                                reply:(void (^)(NSError *error))reply;
+                                reply:(void (^)(BOOL, NSArray<NSError *> *error))reply;
 - (void)databaseEventsPending:(void (^)(NSArray<SNTStoredEvent *> *events))reply;
 - (void)databaseRemoveEventsWithIDs:(NSArray *)ids;
 - (void)retrieveAllExecutionRules:(void (^)(NSArray<SNTRule *> *rules, NSError *error))reply;

Source/common/SNTXPCControlInterface.mm
@@ -61,6 +61,12 @@ NSString *const kBundleID = @"com.northpolesec.santa.daemon";
       argumentIndex:1
             ofReply:NO];
 
+  [r setClasses:[NSSet setWithObjects:[NSArray class], [NSError class], nil]
+        forSelector:@selector(databaseRuleAddExecutionRules:
+                                            fileAccessRules:ruleCleanup:source:reply:)
+      argumentIndex:1
+            ofReply:YES];
+
   [r setClasses:[NSSet setWithObjects:[NSArray class], [SNTRule class], nil]
         forSelector:@selector(retrieveAllExecutionRules:)
       argumentIndex:0

Source/santad/DataLayer/SNTRuleTable.h
@@ -94,21 +94,21 @@
 ///  @param executionRules Array of SNTRule objects to add.
 ///  @param fileAccessRules Array of SNTFileAccessRule objects to add.
 ///  @param ruleCleanup Rule cleanup type to perform (e.g. all, none, non-transitive).
-///  @param error When returning NO, will be filled with appropriate error.
+///  @param errors When returning NO, will be filled with an array of errors.
 ///  @return YES if adding all rules passed, NO if any were rejected.
 ///
 - (BOOL)addExecutionRules:(NSArray<SNTRule *> *)executionRules
           fileAccessRules:(NSArray<SNTFileAccessRule *> *)fileAccessRules
               ruleCleanup:(SNTRuleCleanup)cleanupType
-                    error:(NSError **)error;
+                   errors:(NSArray<NSError *> **)errors;
 
 ///
-/// Wrapper for `addExecutionRules:fileAccessRules:ruleCleanup:error:` when there are no
+/// Wrapper for `addExecutionRules:fileAccessRules:ruleCleanup:errors:` when there are no
 /// file access rules to add.
 ///
 - (BOOL)addExecutionRules:(NSArray<SNTRule *> *)rules
               ruleCleanup:(SNTRuleCleanup)cleanupType
-                    error:(NSError **)error;
+                   errors:(NSArray<NSError *> **)errors;
 
 ///
 ///  Checks the given array of rules to see if adding any of them to the rules database would

Source/santad/DataLayer/SNTRuleTable.mm
@@ -511,7 +511,7 @@ static void addPathsFromDefaultMuteSet(NSMutableSet *criticalPaths) {
 
 - (BOOL)addFileAccessRules:(NSArray<SNTFileAccessRule *> *)fileAccessRules
                       toDB:(FMDatabase *)db
-                     error:(NSError **)error {
+                    errors:(NSMutableArray<NSError *> *)errors {
   for (SNTFileAccessRule *rule in fileAccessRules) {
     // FAA rules must:
     // 1. Be the right type
@@ -520,30 +520,29 @@ static void addPathsFromDefaultMuteSet(NSMutableSet *criticalPaths) {
     if (![rule isKindOfClass:[SNTFileAccessRule class]] || rule.name.length == 0 ||
         ((rule.state == SNTFileAccessRuleStateAdd && rule.details.length == 0) &&
          (rule.state != SNTFileAccessRuleStateRemove))) {
-      [SNTError populateError:error
-                     withCode:SNTErrorCodeRuleInvalid
-                      message:@"File access rule array contained invalid entry"
-                       detail:rule.description];
+      [errors
+          addObject:[SNTError createErrorWithCode:SNTErrorCodeRuleInvalid
+                                          message:@"File access rule array contained invalid entry"
+                                           detail:rule.description]];
       return NO;
     }
 
     if (rule.state == SNTFileAccessRuleStateRemove) {
       if (![db executeUpdate:@"DELETE FROM file_access_rules WHERE name=?", rule.name]) {
-        [SNTError populateError:error
-                       withCode:SNTErrorCodeRemoveRuleFailed
-                        message:@"A database error occurred while deleting a file access rule"
-                         detail:[db lastErrorMessage]];
+        [errors addObject:[SNTError createErrorWithCode:SNTErrorCodeRemoveRuleFailed
+                                                message:@"A database error occurred while deleting "
+                                                        @"a file access rule"
+                                                 detail:[db lastErrorMessage]]];
         return NO;
       }
     } else {
       if (![db executeUpdate:@"INSERT OR REPLACE INTO file_access_rules (name, rule_data) "
                              @"VALUES (?, ?)",
                              rule.name, rule.details]) {
-        [SNTError
-            populateError:error
-                 withCode:SNTErrorCodeInsertOrReplaceRuleFailed
-                  message:@"A database error occurred while inserting/replacing a file access rule"
-                   detail:[db lastErrorMessage]];
+        [errors addObject:[SNTError createErrorWithCode:SNTErrorCodeInsertOrReplaceRuleFailed
+                                                message:@"A database error occurred while "
+                                                        @"inserting/replacing a file access rule"
+                                                 detail:[db lastErrorMessage]]];
         return NO;
       }
     }
@@ -554,24 +553,26 @@ static void addPathsFromDefaultMuteSet(NSMutableSet *criticalPaths) {
 
 - (BOOL)addExecutionRules:(NSArray<SNTRule *> *)executionRules
                      toDB:(FMDatabase *)db
-                    error:(NSError **)error {
+                   errors:(NSMutableArray<NSError *> *)errors {
   for (SNTRule *rule in executionRules) {
     if (![rule isKindOfClass:[SNTRule class]] || rule.identifier.length == 0 ||
         rule.state == SNTRuleStateUnknown || rule.type == SNTRuleTypeUnknown) {
-      [SNTError populateError:error
-                     withCode:SNTErrorCodeRuleInvalid
-                      message:@"Execution rule array contained invalid entry"
-                       detail:rule.description];
+      [errors
+          addObject:[SNTError createErrorWithCode:SNTErrorCodeRuleInvalid
+                                          message:@"Execution rule array contained invalid entry"
+                                           detail:rule.description]];
+
       return NO;
     }
 
     if (rule.state == SNTRuleStateCEL && _celEvaluator) {
       auto celExpr = _celEvaluator->Compile(santa::NSStringToUTF8StringView(rule.celExpr));
       if (!celExpr.ok()) {
-        [SNTError populateError:error
-                       withCode:SNTErrorCodeRuleInvalidCELExpression
-                        message:@"Rule array contained rule with invalid CEL expression"
-                         detail:santa::StringToNSString(celExpr.status().message())];
+        [errors addObject:
+                    [SNTError
+                        createErrorWithCode:SNTErrorCodeRuleInvalidCELExpression
+                                    message:@"Rule array contained rule with invalid CEL expression"
+                                     detail:santa::StringToNSString(celExpr.status().message())]];
         continue;
       }
     }
@@ -579,10 +580,10 @@ static void addPathsFromDefaultMuteSet(NSMutableSet *criticalPaths) {
     if (rule.state == SNTRuleStateRemove) {
       if (![db executeUpdate:@"DELETE FROM execution_rules WHERE identifier=? AND type=?",
                              rule.identifier, @(rule.type)]) {
-        [SNTError populateError:error
-                       withCode:SNTErrorCodeRemoveRuleFailed
-                        message:@"A database error occurred while deleting a rule"
-                         detail:[db lastErrorMessage]];
+        [errors addObject:[SNTError
+                              createErrorWithCode:SNTErrorCodeRemoveRuleFailed
+                                          message:@"A database error occurred while deleting a rule"
+                                           detail:[db lastErrorMessage]]];
         return NO;
       }
     } else {
@@ -592,10 +593,10 @@ static void addPathsFromDefaultMuteSet(NSMutableSet *criticalPaths) {
                              @"VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
                              rule.identifier, @(rule.state), @(rule.type), rule.customMsg,
                              rule.customURL, @(rule.timestamp), rule.comment, rule.celExpr]) {
-        [SNTError populateError:error
-                       withCode:SNTErrorCodeInsertOrReplaceRuleFailed
-                        message:@"A database error occurred while inserting/replacing a rule"
-                         detail:[db lastErrorMessage]];
+        [errors addObject:[SNTError createErrorWithCode:SNTErrorCodeInsertOrReplaceRuleFailed
+                                                message:@"A database error occurred while "
+                                                        @"inserting/replacing a rule"
+                                                 detail:[db lastErrorMessage]]];
         return NO;
       }
     }
@@ -606,28 +607,29 @@ static void addPathsFromDefaultMuteSet(NSMutableSet *criticalPaths) {
 
 - (BOOL)addExecutionRules:(NSArray<SNTRule *> *)executionRules
               ruleCleanup:(SNTRuleCleanup)cleanupType
-                    error:(NSError **)error {
+                   errors:(NSArray<NSError *> **)errors {
   return [self addExecutionRules:executionRules
                  fileAccessRules:nil
                      ruleCleanup:cleanupType
-                           error:error];
+                          errors:errors];
 }
 
 - (BOOL)addExecutionRules:(NSArray<SNTRule *> *)executionRules
           fileAccessRules:(NSArray<SNTFileAccessRule *> *)fileAccessRules
               ruleCleanup:(SNTRuleCleanup)cleanupType
-                    error:(NSError **)error {
+                   errors:(NSArray<NSError *> **)errors {
   // Only accept an empty rules array if the cleanup-type is not none.
   if (executionRules.count == 0 && fileAccessRules.count == 0 &&
       cleanupType == SNTRuleCleanupNone) {
-    [SNTError populateError:error
-                   withCode:SNTErrorCodeEmptyRuleArray
-                     format:@"Empty execution and file access rule arrays"];
+    if (errors) {
+      *errors = @[ [SNTError createErrorWithCode:SNTErrorCodeEmptyRuleArray
+                                          format:@"Empty execution and file access rule arrays"] ];
+    }
     return NO;
   }
 
   __block BOOL failed = NO;
-  __block NSError *blockErr;
+  __block NSMutableArray<NSError *> *blockErrors = [NSMutableArray array];
   __block NSString *faaRulesHashBefore;
   __block NSString *faaRulesHashAfter;
   __block int64_t faaRuleCount = 0;
@@ -657,12 +659,12 @@ static void addPathsFromDefaultMuteSet(NSMutableSet *criticalPaths) {
         break;
     }
 
-    if (![self addExecutionRules:executionRules toDB:db error:&blockErr]) {
+    if (![self addExecutionRules:executionRules toDB:db errors:blockErrors]) {
       *rollback = failed = YES;
       return;
     }
 
-    if (![self addFileAccessRules:fileAccessRules toDB:db error:&blockErr]) {
+    if (![self addFileAccessRules:fileAccessRules toDB:db errors:blockErrors]) {
       *rollback = failed = YES;
       return;
     }
@@ -675,8 +677,8 @@ static void addPathsFromDefaultMuteSet(NSMutableSet *criticalPaths) {
     faaRuleCount = [self fileAccessRuleCountSerialized:db];
   }];
 
-  if (blockErr && error) {
-    *error = blockErr;
+  if (blockErrors.count > 0 && errors) {
+    *errors = [blockErrors copy];
   }
 
   // If the DB updated successfully, call the "rules changed" callback if appropriate

commit ef6761e8c8451799ab53c50986dda76181eb76df

Fix issue when manually managing rules with santactl rule (unreleased) (#701)
This fixes an issue introduced by #695

Source/santactl/Commands/SNTCommandRule.mm
@@ -16,6 +16,7 @@
 #import <CommonCrypto/CommonDigest.h>
 #import <Foundation/Foundation.h>
 #import <Kernel/kern/cs_blobs.h>
+#include <stdlib.h>
 
 #import "Source/common/MOLCertificate.h"
 #import "Source/common/MOLCodesignChecker.h"
@@ -292,14 +293,21 @@ REGISTER_COMMAND_NAME(@"rule")
                                source:SNTRuleAddSourceSantactl
                                 reply:^(BOOL success, NSArray<NSError *> *errors) {
                                   if (success) {
-                                    TEE_LOGE(@"Rules were deleted, with the following warnings:");
+                                    TEE_LOGI(@"Rules were successfully deleted.");
+                                    if (errors.count > 0) {
+                                      TEE_LOGE(@"The following warnings were emitted:");
+                                      for (NSError *e in errors) {
+                                        TEE_LOGW(@"\t%@", e.localizedDescription);
+                                      }
+                                    }
                                   } else {
                                     TEE_LOGE(@"Failed to delete rules:");
+                                    for (NSError *e in errors) {
+                                      TEE_LOGE(@"\t%@", e.localizedDescription);
+                                    }
                                   }
-                                  for (NSError *e in errors) {
-                                    TEE_LOGE(@"\t%@", e.localizedDescription);
-                                  }
-                                  exit(EXIT_FAILURE);
+
+                                  exit(success == NO ? EXIT_FAILURE : EXIT_SUCCESS);
                                 }];
     exit(EXIT_SUCCESS);
   }
@@ -401,51 +409,40 @@ REGISTER_COMMAND_NAME(@"rule")
                         ruleCleanup:SNTRuleCleanupNone
                              source:SNTRuleAddSourceSantactl
                               reply:^(BOOL success, NSArray<NSError *> *errors) {
-                                if (!errors) {
+                                if (!success) {
                                   TEE_LOGE(@"Failed to modify rules:");
                                   for (NSError *e in errors) {
                                     TEE_LOGE(@"\t%@", e.localizedFailureReason);
                                   }
-                                  exit(1);
-                                } else {
-                                  if (errors.count > 0) {
-                                    TEE_LOGE(@"Rules were modified but with the following issues:");
-                                    for (NSError *e in errors) {
-                                      TEE_LOGE(@"\t%@", e.localizedFailureReason);
-                                    }
-                                  }
-                                  NSString *ruleType;
-                                  switch (newRule.type) {
-                                    case SNTRuleTypeCertificate:
-                                      ruleType = @"Certificate SHA-256";
-                                      break;
-                                    case SNTRuleTypeBinary: {
-                                      ruleType = @"SHA-256";
-                                      break;
-                                    }
-                                    case SNTRuleTypeTeamID: {
-                                      ruleType = @"Team ID";
-                                      break;
-                                    }
-                                    case SNTRuleTypeSigningID: {
-                                      ruleType = @"Signing ID";
-                                      break;
-                                    }
-                                    case SNTRuleTypeCDHash: {
-                                      ruleType = @"CDHash";
-                                      break;
-                                    }
-                                    default: ruleType = @"(Unknown type)";
-                                  }
-                                  if (newRule.state == SNTRuleStateRemove) {
-                                    printf("Removed rule for %s: %s.\n", [ruleType UTF8String],
-                                           [newRule.identifier UTF8String]);
-                                  } else {
-                                    printf("Added rule for %s: %s.\n", [ruleType UTF8String],
-                                           [newRule.identifier UTF8String]);
-                                  }
-                                  exit(0);
+                                  exit(EXIT_FAILURE);
                                 }
+
+                                if (errors.count > 0) {
+                                  TEE_LOGW(@"Rules were modified but with the following issues:");
+                                  for (NSError *e in errors) {
+                                    TEE_LOGW(@"\t%@", e.localizedFailureReason);
+                                  }
+                                }
+
+                                NSString *ruleType;
+                                switch (newRule.type) {
+                                  case SNTRuleTypeCertificate:
+                                    ruleType = @"Certificate SHA-256";
+                                    break;
+                                  case SNTRuleTypeBinary: ruleType = @"SHA-256"; break;
+                                  case SNTRuleTypeTeamID: ruleType = @"Team ID"; break;
+                                  case SNTRuleTypeSigningID: ruleType = @"Signing ID"; break;
+                                  case SNTRuleTypeCDHash: ruleType = @"CDHash"; break;
+                                  default: ruleType = @"(Unknown type)"; break;
+                                }
+                                if (newRule.state == SNTRuleStateRemove) {
+                                  printf("Removed rule for %s: %s.\n", [ruleType UTF8String],
+                                         [newRule.identifier UTF8String]);
+                                } else {
+                                  printf("Added rule for %s: %s.\n", [ruleType UTF8String],
+                                         [newRule.identifier UTF8String]);
+                                }
+                                exit(EXIT_SUCCESS);
                               }];
 }
 