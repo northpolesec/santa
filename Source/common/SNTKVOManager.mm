e5ebb5e151144b8914b33c1c70b56dbeebf6c3c3
Matt W <436037+mlw@users.noreply.github.com>
2026-03-19 13:17:37 -0400

    Use earliest modified year for NPS copyright

--- Source/common/SNTKVOManager.mm
+++ Source/common/SNTKVOManager.mm
@@ -1,5 +1,5 @@
 /// Copyright 2022 Google LLC
-/// Copyright 2026 North Pole Security, Inc.
+/// Copyright 2024 North Pole Security, Inc.
 ///
 /// Licensed under the Apache License, Version 2.0 (the "License");
 /// you may not use this file except in compliance with the License.
